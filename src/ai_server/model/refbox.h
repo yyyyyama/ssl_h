16addb351543785831f850a47612a7721c837637
hatokani2 <atsuya887@gmail.com>
2016-12-25 21:32:26 +0900

    cording style corrected const added initialization of team_yellow_ team_blue_ corrected

--- src/ai_server/model/refbox.h
+++ src/ai_server/model/refbox.h
@@ -1,73 +1,101 @@
-#include <iostream>
+#include <string>
+#ifndef INCLUDE_GUARD_REFBOX
+#define INCLUDE_GUARD_REFBOX
 
-namespace ai_server{
-    namespace model{
+namespace ai_server {
+namespace model {
 
-        class refbox{
-            class team_info{
-                std::string name_;
-                int score_, goalie_;
-                int red_cards_, yellow_cards_, yellow_card_times_;
-                int timeouts_, timeout_time_;
+class refbox {
+  class team_info {
+    std::string name_;
+    int score_;
+    int goalie_;
+    int red_cards_;
+    int yellow_cards_;
+    int yellow_card_times_;
+    int timeouts_;
+    int timeout_time_;
 
-            public:
-                team_info(std::string name);
-                std::string name();
-                int score();
-                int goalie();
-                int red_cards();
-                int yellow_cards();
-                int yellow_card_times();
-                int timeouts();
-                int timeout_time();
-                void set_score(int value);
-                void set_goalie(int value);
-                void set_red_cards(int value);
-                void set_yellow_cards(int value);
-                void set_yellow_card_times(int value);
-                void set_timeouts(int value);
-                void set_timeout_time(int value);
+  public:
+    team_info(std::string name);
+    std::string name() const;
+    int score() const;
+    int goalie() const;
+    int red_cards() const;
+    int yellow_cards() const;
+    int yellow_card_times() const;
+    int timeouts() const;
+    int timeout_time() const;
+    void set_score(int value);
+    void set_goalie(int value);
+    void set_red_cards(int value);
+    void set_yellow_cards(int value);
+    void set_yellow_card_times(int value);
+    void set_timeouts(int value);
+    void set_timeout_time(int value);
+  };
 
-            };
-        
-            enum class stage_name {NORMAL_FIRST_HALF_PRE, NORMAL_FIRST_HALF,
-                                    NORMAL_HALF_TIME, NORMAL_SECOND_HALF_PRE,
-                                    NORMAL_SECOND_HALF, EXTRA_TIME_BREAK,
-                                    EXTRA_FIRST_HALF_PRE, EXTRA_FIRST_HALF,
-                                    EXTRA_HALF_TIME, EXTRA_SECOND_HALF_PRE,
-                                    EXTRA_SECOND_HALF, PENALTY_SHOOTOUT_BREAK,
-                                    PENALTY_SHOOTOUT, POST_GAME};
-            enum class game_command {HALF, STOP, NORMAL_START, FORCE_START,
-                                    PREPARE_KICKOFF_YELLOW, PREPARE_KICKOFF_BLUE,
-                                    PREPARE_PENALTY_YELLOW, PREPARE_PENALTY_BLUE,
-                                    DIRECT_FREE_YELLOW, DIRECT_FREE_BLUE,
-                                    INDIRECT_FREE_YELLOW, INDIRECT_FREE_BLUE,
-                                    TIMEOUT_YELLOW, TIMEOUT_BLUE,
-                                    GOAL_YELLOW, GOAL_BLUE,
-                                    BALL_PLACEMENT_YELLOW, BALL_PLACEMENT_BLUE};
-            const int NORMAL_FIRST_HALF_PRE = 0;
-            const int HALF = 0;
-            int packet_timestamp_, stage_time_left_;
-            int stage_;
-            int command_;
-            team_info team_yellow_ = {"yellow"}, team_blue_ = {"blue"};
-    
-        public:
-            refbox();
-            int packet_timestamp();
-            int stage_time_left();
-            int stage();
-            int command();
-            team_info team_yellow();
-            team_info team_blue();
-            void set_packet_timestamp(int value);
-            void set_stage_time_left(int value);
-            void set_stage(int value);
-            void set_command(int value);
-            void set_team_yellow(team_info value);
-            void set_team_blue(team_info value);
+  enum class stage_name {
+    normal_first_half_pre,
+    normal_first_half,
+    normal_half_time,
+    normal_second_half_pre,
+    normal_second_half,
+    extra_time_brake,
+    extra_first_half_pre,
+    extra_first_half,
+    extra_half_time,
+    extra_second_half_pre,
+    extra_second_half,
+    penalty_shootout_break,
+    penalty_shootout,
+    post_game
+  };
+  enum class game_command {
+    half,
+    stop,
+    normal_start,
+    force_start,
+    prepare_kickoff_yellow,
+    prepare_kickoff_blue,
+    prepare_penalty_yellow,
+    prepare_penalty_blue,
+    direct_free_yellow,
+    direct_free_blue,
+    indirect_free_yellow,
+    indirect_free_blue,
+    timeout_yellow,
+    timeout_blue,
+    goal_yellow,
+    goal_blue,
+    ball_placement_yellow,
+    ball_placement_blue
+  };
+  const int normal_first_half_pre = 0;
+  const int half = 0;
+  int packet_timestamp_;
+  int stage_time_left_;
+  stage_name stage_;
+  game_command command_;
+  team_info team_yellow_;
+  team_info team_blue_;
 
-
-        };
-    }
+public:
+  refbox();
+  int packet_timestamp() const;
+  int stage_time_left() const;
+  stage_name stage() const;
+  game_command command() const;
+  team_info team_yellow() const;
+  team_info team_blue() const;
+  void set_packet_timestamp(int value);
+  void set_stage_time_left(int value);
+  void set_stage(stage_name value);
+  void set_command(game_command value);
+  void set_team_yellow(team_info value);
+  void set_team_blue(team_info value);
+};
 }
+}
+
+#endif
