73d18b9fe632398c66dd856c8425980c5ba5bfe8
KondoTakumi <kontyanzhihg@gmail.com>
2016-12-25 21:22:55 +0900

    3-ball-datatype

---
+++ src/ai_server/model/ball.h
@@ -0,0 +1,34 @@
+#ifndef AI_SERVER_MODEL_BALL_H
+#define AI_SERVER_MODEL_BALL_H
+namespace ai_server {
+namespace model {
+class ball {
+private:
+  int x_;
+  int y_;
+  int z_;
+  int vx_;
+  int vy_;
+  int ax_;
+  int ay_;
+
+public:
+  ball(int x, int y, int z);
+  int x() const;
+  int y() const;
+  int z() const;
+  int vx() const;
+  int vy() const;
+  int ax() const;
+  int ay() const;
+  void set_x(int x);
+  void set_y(int y);
+  void set_z(int z);
+  void set_vx(int vx);
+  void set_vy(int vy);
+  void set_ax(int ax);
+  void set_ay(int ay);
+};
+}
+}
+#endif
