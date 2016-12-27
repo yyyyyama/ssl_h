6f283814c5b35d38029b4485c0e096dc7ca868a7
Sakyo <sakyo.hashimoto@gmail.com>
2016-12-27 15:50:00 +0900

    Fix ./test/model/robot.cc

--- test/model/robot.cc
+++ test/model/robot.cc
@@ -2,6 +2,7 @@
 #define BOOST_TEST_MODULE robotdata_Test
 
 #include <boost/test/unit_test.hpp>
+#include "src/ai_server/model/robot.h"
 
 
 BOOST_AUTO_TEST_SUITE(robotdata)
@@ -10,7 +11,6 @@ BOOST_AUTO_TEST_CASE(test001) {
 	
 	// constructor test
   ai_server::model::robot rob{1,2,3};
-  
   
   // get value test
   BOOST_TEST(rob.id() == 1);
