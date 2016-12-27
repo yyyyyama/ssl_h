8c5ee5cfd1c4c72ef32fa855f93a0439d7aee0b9
Tosainu <tosainu.maple@gmail.com>
2016-12-27 20:34:26 +0900

    SSL-Visionのデータを受信するクラスを追加

---
+++ src/ai_server/receiver/vision.cc
@@ -0,0 +1,41 @@
+#include <functional>
+#include "vision.h"
+
+namespace ai_server {
+namespace receiver {
+
+vision::vision(boost::asio::io_service& io_service, const std::string& listen_addr,
+               const std::string& multicast_addr, short port)
+    : receiver_(io_service, listen_addr, multicast_addr, port) {
+  // Google Protocol Buffersライブラリのバージョンをチェックする
+  // 互換性のないバージョンが使われていた場合は例外吐いて落ちる()
+  GOOGLE_PROTOBUF_VERIFY_VERSION;
+
+  // multicastクライアントとparse_packet()をつなげる
+  receiver_.on_receive(
+      std::bind(&vision::parse_packet, this, std::placeholders::_1, std::placeholders::_2));
+}
+
+boost::signals2::connection vision::on_receive(const receive_signal_t::slot_type& slot) {
+  return on_receive_.connect(slot);
+}
+
+boost::signals2::connection vision::on_error(const error_signal_t::slot_type& slot) {
+  return on_error_.connect(slot);
+}
+
+void vision::parse_packet(const util::net::multicast::receiver::buffer_t& buffer,
+                          std::size_t size) {
+  ssl_protos::vision::Packet packet;
+
+  // パケットをパース
+  if (packet.ParseFromArray(buffer.data(), size)) {
+    // 成功したら登録された関数を呼び出す
+    on_receive_(packet);
+  } else {
+    on_error_();
+  }
+}
+
+} // namespace receiver
+} // namespace ai_server
---
+++ src/ai_server/receiver/vision.h
@@ -0,0 +1,51 @@
+#ifndef AI_SERVER_RECEIVER_VISION_H
+#define AI_SERVER_RECEIVER_VISION_H
+
+#include <string>
+#include <boost/asio.hpp>
+#include <boost/signals2.hpp>
+
+#include "ai_server/util/net/multicast/receiver.h"
+
+#include "ssl-protos/vision/wrapperpacket.pb.h"
+
+namespace ai_server {
+namespace receiver {
+
+/// @class   vision
+/// @brief   SSL-Visionからデータを受信するクラス
+class vision {
+  // データ受信時に呼ぶシグナルの型
+  using receive_signal_t = boost::signals2::signal<void(const ssl_protos::vision::Packet&)>;
+  // エラー時に呼ぶシグナルの型
+  using error_signal_t = boost::signals2::signal<void(void)>;
+
+public:
+  /// @brief                  コンストラクタ
+  /// @param listen_addr      通信に使うインターフェースのIPアドレス
+  /// @param multicast_addr   マルチキャストアドレス
+  /// @param port             ポート
+  vision(boost::asio::io_service& io_service, const std::string& listen_addr,
+         const std::string& multicast_addr, short port);
+
+  /// @brief                  データ受信時に slot が呼ばれるようにする
+  /// @param slot             データ受信時に呼びたい関数オブジェクト
+  boost::signals2::connection on_receive(const receive_signal_t::slot_type& slot);
+
+  /// @brief                  エラー時に slot が呼ばれるようにする
+  /// @param slot             エラー時に呼びたい関数オブジェクト
+  boost::signals2::connection on_error(const error_signal_t::slot_type& slot);
+
+private:
+  void parse_packet(const util::net::multicast::receiver::buffer_t& buffer, std::size_t size);
+
+  receive_signal_t on_receive_;
+  error_signal_t on_error_;
+
+  util::net::multicast::receiver receiver_;
+};
+
+} // namespace receiver
+} // namespace ai_server
+
+#endif // AI_SERVER_RECEIVER_VISION_H
