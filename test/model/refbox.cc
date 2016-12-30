ee54288be13640ae7a862a00d21735645931e097
hatokani2 <atsuya887@gmail.com>
2016-12-30 19:41:04 +0900

    team_info->refboxの順でテストするように修正 team_infoのセッターのテストでの引数を変更 set_team_yellow()とset_team_blue()をrefboxのセッターでテストするよう修正

--- test/model/refbox.cc
+++ test/model/refbox.cc
@@ -7,8 +7,42 @@ BOOST_TEST_DONT_PRINT_LOG_VALUE(ai_server::model::refbox::game_command)
 
 BOOST_AUTO_TEST_SUITE(refbox)
 
-// refbox getter and initialization check
+// team_info getter and initialization check
 BOOST_AUTO_TEST_CASE(test01) {
+  ai_server::model::refbox::team_info team_test{"t"};
+
+  BOOST_TEST(team_test.score() == 0);
+  BOOST_TEST(team_test.goalie() == 0);
+  BOOST_TEST(team_test.red_cards() == 0);
+  BOOST_TEST(team_test.yellow_cards() == 0);
+  BOOST_TEST(team_test.yellow_card_times() == 0);
+  BOOST_TEST(team_test.timeouts() == 0);
+  BOOST_TEST(team_test.timeout_time() == 0);
+}
+
+// team_info setter check
+BOOST_AUTO_TEST_CASE(test02) {
+  ai_server::model::refbox ref{};
+  ai_server::model::refbox::team_info team_test{"t"};
+
+  team_test.set_score(1);
+  BOOST_TEST(team_test.score() == 1);
+  team_test.set_goalie(2);
+  BOOST_TEST(team_test.goalie() == 2);
+  team_test.set_red_cards(3);
+  BOOST_TEST(team_test.red_cards() == 3);
+  team_test.set_yellow_cards(4);
+  BOOST_TEST(team_test.yellow_cards() == 4);
+  team_test.set_yellow_card_times(5);
+  BOOST_TEST(team_test.yellow_card_times() == 5);
+  team_test.set_timeouts(6);
+  BOOST_TEST(team_test.timeouts() == 6);
+  team_test.set_timeout_time(7);
+  BOOST_TEST(team_test.timeout_time() == 7);
+}
+
+// refbox getter and initialization check
+BOOST_AUTO_TEST_CASE(test03) {
   ai_server::model::refbox ref{};
 
   BOOST_TEST(ref.packet_timestamp() == 0);
@@ -20,8 +54,9 @@ BOOST_AUTO_TEST_CASE(test01) {
 }
 
 // refbox setter check
-BOOST_AUTO_TEST_CASE(test02) {
+BOOST_AUTO_TEST_CASE(test04) {
   ai_server::model::refbox ref{};
+  ai_server::model::refbox::team_info team_test{"t"};
 
   ref.set_packet_timestamp(1);
   BOOST_TEST(ref.packet_timestamp() == 1);
@@ -31,40 +66,6 @@ BOOST_AUTO_TEST_CASE(test02) {
   BOOST_TEST(ref.stage_time_left() == 1);
   ref.set_command(ai_server::model::refbox::game_command::stop);
   BOOST_TEST(ref.command() == ai_server::model::refbox::game_command::stop);
-}
-
-// team_info getter and initialization check
-BOOST_AUTO_TEST_CASE(test03) {
-  ai_server::model::refbox ref{};
-
-  BOOST_TEST(ref.team_yellow().score() == 0);
-  BOOST_TEST(ref.team_yellow().goalie() == 0);
-  BOOST_TEST(ref.team_yellow().red_cards() == 0);
-  BOOST_TEST(ref.team_yellow().yellow_cards() == 0);
-  BOOST_TEST(ref.team_yellow().yellow_card_times() == 0);
-  BOOST_TEST(ref.team_yellow().timeouts() == 0);
-  BOOST_TEST(ref.team_yellow().timeout_time() == 0);
-}
-
-// team_info setter check
-BOOST_AUTO_TEST_CASE(test04) {
-  ai_server::model::refbox ref{};
-  ai_server::model::refbox::team_info team_test{"t"};
-
-  team_test.set_score(1);
-  BOOST_TEST(team_test.score() == 1);
-  team_test.set_goalie(1);
-  BOOST_TEST(team_test.goalie() == 1);
-  team_test.set_red_cards(1);
-  BOOST_TEST(team_test.red_cards() == 1);
-  team_test.set_yellow_cards(1);
-  BOOST_TEST(team_test.yellow_cards() == 1);
-  team_test.set_yellow_card_times(1);
-  BOOST_TEST(team_test.yellow_card_times() == 1);
-  team_test.set_timeouts(1);
-  BOOST_TEST(team_test.timeouts() == 1);
-  team_test.set_timeout_time(1);
-  BOOST_TEST(team_test.timeout_time() == 1);
   ref.set_team_yellow(team_test);
   BOOST_TEST(ref.team_yellow().name() == team_test.name());
   ref.set_team_blue(team_test);
