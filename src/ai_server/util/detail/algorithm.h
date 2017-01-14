6946551d7e031149d6a712088aec1752278892cb
Tosainu <tosainu.maple@gmail.com>
2017-01-14 21:20:49 +0900

    has_popを実装

--- src/ai_server/util/detail/algorithm.h
+++ src/ai_server/util/detail/algorithm.h
@@ -24,6 +24,23 @@ public:
 template <class T>
 static constexpr bool has_front_v = has_front<T>::value;
 
+/// @class   has_pop
+/// @brief   T がメンバ関数 pop() を持っているか調べるメタ関数
+template <class T>
+class has_pop {
+  template <class U>
+  static auto check(U&& x) -> decltype(x.pop(), std::true_type{});
+
+  template <class U>
+  static auto check(...) -> std::false_type;
+
+public:
+  static constexpr bool value = decltype(check<T>(std::declval<T>()))::value;
+};
+
+template <class T>
+static constexpr bool has_pop_v = has_pop<T>::value;
+
 /// @class   has_top
 /// @brief   T がメンバ関数 top() を持っているか調べるメタ関数
 template <class T>
