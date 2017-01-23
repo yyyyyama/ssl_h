eef14212d244d0b06c834a5d3f671a0defd0416c
Tosainu <tosainu.maple@gmail.com>
2017-01-23 23:33:22 +0900

    ランダムな値を使ったテストを追加

--- test/util/net/multicast.cc
+++ test/util/net/multicast.cc
@@ -2,8 +2,11 @@
 #define BOOST_TEST_MODULE multicast_test
 
 #include <chrono>
+#include <limits>
+#include <random>
 #include <string>
 #include <thread>
+#include <vector>
 #include <boost/asio.hpp>
 #include <boost/test/unit_test.hpp>
 
@@ -70,6 +73,35 @@ BOOST_AUTO_TEST_CASE(send_and_receive) {
     BOOST_TEST(std::get<1>(result) == s1.length());
     const auto s2 = std::string(std::cbegin(std::get<0>(result)), std::get<1>(result));
     BOOST_TEST(s2 == s1);
+  }
+
+  // 乱数生成機を初期化
+  std::mt19937 mt{std::random_device{}()};
+
+  for (auto i = 0u; i < 1024; ++i) {
+    receiver_wrapper wrapper{&receiver::on_receive, r};
+
+    using value_type = typename receiver::buffer_t::value_type;
+
+    // ランダムな長さ[1, 4096]のランダムなデータの入った配列を生成
+    const auto size = std::uniform_int_distribution<std::size_t>(1, receiver::buffer_size)(mt);
+    const auto v1   = [&mt, size] {
+      std::vector<value_type> v(size);
+      std::uniform_int_distribution<value_type> d(std::numeric_limits<value_type>::min(),
+                                                  std::numeric_limits<value_type>::max());
+      std::generate(v.begin(), v.end(), [&mt, &d] { return d(mt); });
+      return v;
+    }();
+    s.send(boost::asio::buffer(v1));
+
+    // 受信したデータを取得
+    const auto result = wrapper.result();
+
+    // 受信したデータが送信したものと一致するか
+    BOOST_TEST(std::get<1>(result) == v1.size());
+    const auto& buf = std::get<0>(result);
+    const std::vector<value_type> v2(buf.cbegin(), buf.cbegin() + v1.size());
+    BOOST_TEST(v2 == v1, boost::test_tools::per_element());
   }
 
   // 受信の終了
