a2a2fc443df7ec344d867a3ab839eefdf12f1af1
Tosainu <tosainu.maple@gmail.com>
2017-01-24 17:38:41 +0900

    Vision receiverのテストをslot_testing_helperを使ったものに変更

--- test/receiver/vision.cc
+++ test/receiver/vision.cc
@@ -8,6 +8,8 @@
 #include <boost/asio.hpp>
 #include <boost/test/unit_test.hpp>
 
+#include "../util/slot_testing_helper.h"
+
 #include "ai_server/receiver/vision.h"
 #include "ai_server/util/net/multicast/sender.h"
 
@@ -35,10 +37,6 @@ BOOST_AUTO_TEST_CASE(send_and_receive) {
   // listen_addr = 0.0.0.0, multicast_addr = 224.5.23.3, port = 10008
   vision v(io_service, "0.0.0.0", "224.5.23.3", 10008);
 
-  // 何か受信したらpromiseに値をセットする
-  std::promise<ssl_protos::vision::Packet> p;
-  v.on_receive([&p](const auto& packet) { p.set_value(packet); });
-
   // 送信クラスの初期化
   // multicast_addr = 224.5.23.3, port = 10008
   sender s(io_service, "224.5.23.3", 10008);
@@ -49,24 +47,28 @@ BOOST_AUTO_TEST_CASE(send_and_receive) {
   // 念の為少し待つ
   std::this_thread::sleep_for(std::chrono::milliseconds(250));
 
-  // ダミーパケットを送信
-  boost::asio::streambuf buf;
-  std::ostream os(&buf);
-  dummy_packet.SerializeToOstream(&os);
-  s.send(buf.data());
+  {
+    slot_testing_helper<ssl_protos::vision::Packet> wrapper{&vision::on_receive, v};
 
-  // データが受信されるのを待つ
-  const auto f = p.get_future().get();
+    // ダミーパケットを送信
+    boost::asio::streambuf buf;
+    std::ostream os(&buf);
+    dummy_packet.SerializeToOstream(&os);
+    s.send(buf.data());
 
-  // 受信したデータがダミーパケットと一致するか確認する
-  BOOST_TEST(f.has_detection());
-  BOOST_TEST(!f.has_geometry());
+    // 受信したデータを取得
+    const auto f = std::get<0>(wrapper.result());
 
-  const auto& d = f.detection();
-  BOOST_TEST(d.frame_number() == dummy_frame.frame_number());
-  BOOST_TEST(d.t_capture() == dummy_frame.t_capture());
-  BOOST_TEST(d.t_sent() == dummy_frame.t_sent());
-  BOOST_TEST(d.camera_id() == dummy_frame.camera_id());
+    // 受信したデータがダミーパケットと一致するか確認する
+    BOOST_TEST(f.has_detection());
+    BOOST_TEST(!f.has_geometry());
+
+    const auto& d = f.detection();
+    BOOST_TEST(d.frame_number() == dummy_frame.frame_number());
+    BOOST_TEST(d.t_capture() == dummy_frame.t_capture());
+    BOOST_TEST(d.t_sent() == dummy_frame.t_sent());
+    BOOST_TEST(d.camera_id() == dummy_frame.camera_id());
+  }
 
   // 受信の終了
   io_service.stop();
