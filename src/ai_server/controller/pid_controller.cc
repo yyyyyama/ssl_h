ca6f34f89dbb8b138df45ba0b22691fa930cdd4d
ToshikiMimura <tm36104.kto.0724@gmail.com>
2017-03-28 11:46:38 +0900

    回転系のpidゲインをxyとは別に設定
    書き方的にx,y,rotateそれぞれのゲインをバラバラに書けるが，
    基本的にxyは同じゲインを用いる
    回転系はxyより大きく出来そうだったため別で設定できるように

--- src/ai_server/controller/pid_controller.cc
+++ src/ai_server/controller/pid_controller.cc
@@ -3,9 +3,9 @@
 namespace ai_server {
 namespace controller {
 
-const double pid_controller::kp_ = 1.0;
-const double pid_controller::ki_ = 0.1;
-const double pid_controller::kd_ = 0.0;
+const double pid_controller::kp_[] = {2.0, 4.0};
+const double pid_controller::ki_[] = {0.2, 0.2};
+const double pid_controller::kd_[] = {0.0, 0.0};
 
 // model::command::velocityの四則演算のオーバロード
 const velocity_t operator+(const velocity_t& vel1, const velocity_t& vel2) {
@@ -75,12 +75,16 @@ velocity_t pid_controller::update(const model::robot& robot, const velocity_t& s
 }
 
 void pid_controller::calculate() {
+  // 計算用にゲイン再計算
+  velocity_t kp = model::command::velocity_t{kp_[0], kp_[0], kp_[1]};
+  velocity_t ki = model::command::velocity_t{ki_[0], ki_[0], ki_[1]};
+  velocity_t kd = model::command::velocity_t{kd_[0], kd_[0], kd_[1]};
   // 双一次変換
   // s=(2/T)*(Z-1)/(Z+1)としてPIDcontrollerを離散化
   // C=Kp+Ki/s+Kds
-  up_[0] = kp_ * e_[0];
-  ui_[0] = ki_ * cycle_ * (e_[0] + e_[1]) / 2 + ui_[1];
-  ud_[0] = 2 * kd_ * (e_[0] - e_[1]) / cycle_ - ud_[1];
+  up_[0] = kp * e_[0];
+  ui_[0] = cycle_ * ki * (e_[0] + e_[1]) / 2 + ui_[1];
+  ud_[0] = 2 * kd * (e_[0] - e_[1]) / cycle_ - ud_[1];
   u_[0]  = u_[1] + cycle_ * (up_[0] + ui_[0] + ud_[0]);
 
   // 値の更新
