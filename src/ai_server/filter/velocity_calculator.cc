c7abbc17eb48722c0b6102268f04010568a6b057
hatokani2 <atsuya887@gmail.com>
2017-03-30 16:49:47 +0900

    M_PIをBoost.Mathのpiに変更

--- src/ai_server/filter/velocity_calculator.cc
+++ src/ai_server/filter/velocity_calculator.cc
@@ -1,4 +1,5 @@
 #include "velocity_calculator.h"
+#include <boost/math/constants/constants.hpp>
 
 namespace ai_server {
 namespace filter {
@@ -17,16 +18,17 @@ void velocity_calculator<model::ball>::apply(
 template <>
 void velocity_calculator<model::robot>::apply(
     model::robot& robot, std::chrono::high_resolution_clock::time_point time) {
+  using namespace boost::math::constants;
   const auto elapsed_time = std::chrono::duration<double>(time - prev_time_);
   robot.set_vx((robot.x() - prev_state_.x()) / elapsed_time.count());
   robot.set_vy((robot.y() - prev_state_.y()) / elapsed_time.count());
-  if(std::abs(robot.theta() - prev_state_.theta()) <= M_PI)
+  if(std::abs(robot.theta() - prev_state_.theta()) <= pi<double>())
     robot.set_omega((robot.theta() - prev_state_.theta()) / elapsed_time.count());
   else{
     if(prev_state_.theta() < 0)
-      robot.set_omega(-(2 * M_PI - std::fabs(robot.theta()) - std::fabs(prev_state_.theta())) / elapsed_time.count());
+      robot.set_omega(-(two_pi<double>() - std::fabs(robot.theta()) - std::fabs(prev_state_.theta())) / elapsed_time.count());
     else
-      robot.set_omega((2 * M_PI - std::fabs(robot.theta()) - std::fabs(prev_state_.theta())) / elapsed_time.count());
+      robot.set_omega((two_pi<double>() - std::fabs(robot.theta()) - std::fabs(prev_state_.theta())) / elapsed_time.count());
   }
   prev_state_ = robot;
   prev_time_  = time;
