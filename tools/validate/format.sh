830c238bed85d4d99d54a204a4ac6b29b0b6c610
Kenta Sato <tosainu.maple@gmail.com>
2017-04-04 20:11:14 +0900

    コードが整形されているかの確認用スクリプトを修正 [skip ci]
    変更前は整形されていないファイルが見つかった時点で終了していたが,
    整形されていないファイルを全て表示してから終了するように変更.

--- tools/validate/format.sh
+++ tools/validate/format.sh
@@ -2,9 +2,13 @@
 
 . "$(dirname "$0")/../is_toplevel.sh"
 
+RETVAL=0
+
 for f in $(git ls-files | grep -E '*\.(cc|h)$'); do
   if ! clang-format "${f}" | diff "${f}" - > /dev/null 2>&1; then
-    echo "Error: Invalid code formatting in ${f}"
-    exit 1
+    echo "Error: Invalid code formatting in ${f}" >&2
+    RETVAL=1
   fi
 done
+
+exit $RETVAL
