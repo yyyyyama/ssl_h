4e28c2e69fdc07b1e45c066977eb496a7d1e04c7
NakayamaSatoru <adlersschanze@gmail.com>
2017-04-07 17:39:02 +0900

    [change]敵機とボールの直線状に自機を配置する計算の変更
    前回の計算方法だと、無駄に量が多く計算ミスの可能性が高かったため変更した。

--- src/ai_server/game/action/marking.cc
+++ src/ai_server/game/action/marking.cc
@@ -18,45 +18,29 @@ model::command marking::execute() {
   const auto enemy_robots  = is_yellow_ ? world_.robots_blue() : world_.robots_yellow();
   const auto& enemy_robot_ = enemy_robots.at(enemy_id_);
   /*必要なパラメータ*/
-  auto opponent_x = 0.0;
-  auto opponent_y = 0.0;
-  auto radius     = 0.0;
-  switch (mode_) {
-    case 0:
-      /*敵機とボールをマーク*/
-      opponent_x = world_.ball().x();
-      opponent_y = world_.ball().y();
-      radius     = 180.0;
-      break;
-    case 1:
-      /*敵機のシュートをマーク
-       * 座標>0が敵陣地			*/
-      opponent_x = -4500.0;
-      opponent_y = 0.0;
-      radius     = 1000.0;
-      break;
-  }
+  const auto radius  = 180;
   const auto enemy_x = enemy_robot_.x();
   const auto enemy_y = enemy_robot_.y();
-  /*計算過程で使用する定数と係数*/
-  const auto constant_1 =
-      (-opponent_x * (enemy_y - opponent_x) + opponent_y * (enemy_x - opponent_x)) /
-      (enemy_x - enemy_x - opponent_x);
-  const auto constant_2 = (enemy_y - opponent_y) / (enemy_x - opponent_x);
+  const auto enemy_d = std::hypot(enemy_x, enemy_y);
+  const auto ball_x  = world_.ball().x();
+  const auto ball_y  = world_.ball().y();
+  const auto ball_d  = std::hypot(ball_x, ball_y);
 
-  const auto coefficient_1 = 1 + std::pow(constant_2, 2);
-  const auto coefficient_2 =
-      -enemy_x / 2 + (constant_2 / 2) * constant_1 - (enemy_y / 2) * constant_2;
-  const auto coefficient_3 =
-      (std::pow(radius, 2) - std::pow(enemy_x, 2) - std::pow(enemy_y, 2)) +
-      (enemy_y / 2) * constant_1 - std::pow(constant_1, 2);
-  /*解の公式より交点を求める*/
-  const auto x = (-coefficient_2 + std::sqrt(std::pow(coefficient_2, 2) -
-                                             4 * coefficient_1 * (-coefficient_3))) /
-                 (2 * coefficient_1);
-  const auto y = x * constant_2 + constant_1;
+  const auto coefficient = radius / std::hypot(enemy_x - ball_x, enemy_y - ball_y);
+
+  auto x = 0.0;
+  auto y = 0.0;
+
+  if (ball_d > enemy_d) {
+    x = ball_x * coefficient + radius / 2;
+    y = ball_y * coefficient + radius / 2;
+  } else {
+    x = enemy_x * coefficient + radius / 2;
+    y = enemy_y * coefficient + radius / 2;
+  }
+
   /*向きをボールの方へ*/
-  const auto theta = ai_server::util::wrap_to_2pi(std::atan2(opponent_x - x, opponent_y - y));
+  const auto theta = ai_server::util::wrap_to_2pi(std::atan2(ball_x - x, ball_y - y));
   /*計算した値を自機にセット*/
   ally_robot.set_position({x, y, theta});
 
