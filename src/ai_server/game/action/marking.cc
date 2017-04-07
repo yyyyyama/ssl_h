83cab9ac36998999bb1b67f64e50f82e03f9b0dc
NakayamaSatoru <adlersschanze@gmail.com>
2017-04-07 18:06:42 +0900

    [add]新しい計算方法でのシュートブロックの追加
    計算を新しくしたのに伴い、シュートブロックを消していたので追加した。

--- src/ai_server/game/action/marking.cc
+++ src/ai_server/game/action/marking.cc
@@ -18,29 +18,49 @@ model::command marking::execute() {
   const auto enemy_robots  = is_yellow_ ? world_.robots_blue() : world_.robots_yellow();
   const auto& enemy_robot_ = enemy_robots.at(enemy_id_);
   /*必要なパラメータ*/
-  const auto radius  = 180;
   const auto enemy_x = enemy_robot_.x();
   const auto enemy_y = enemy_robot_.y();
   const auto enemy_d = std::hypot(enemy_x, enemy_y);
-  const auto ball_x  = world_.ball().x();
-  const auto ball_y  = world_.ball().y();
-  const auto ball_d  = std::hypot(ball_x, ball_y);
 
-  const auto coefficient = radius / std::hypot(enemy_x - ball_x, enemy_y - ball_y);
+  auto radius = 0.0;
+  auto unk_x  = 0.0;
+  auto unk_y  = 0.0;
+  auto dir_x  = 0.0;
+  auto dir_y  = 0.0;
+  auto x      = 0.0;
+  auto y      = 0.0;
 
-  auto x = 0.0;
-  auto y = 0.0;
+  switch (mode_) {
+    case 0:
+      dir_x = unk_x = world_.ball().x();
+      dir_y = unk_y = world_.ball().y();
+      radius        = 200.0;
+      break;
+    case 1:
+      unk_x  = -4500.0;
+      unk_y  = 0.0;
+      dir_x  = enemy_x;
+      dir_y  = enemy_y;
+      radius = 1100.0;
+      break;
+  }
 
+  const auto ball_d = std::hypot(unk_x, unk_y);
+
+  //ボール(ゴール)と敵機の距離から欲しい距離(ロボット1台分)を計算して、その率を出している
+  const auto coefficient = radius / std::hypot(enemy_x - unk_x, enemy_y - unk_y);
+
+  /*ボール(ゴール)と敵機のどちらがより原点より遠くにあるかを判別して、そこから拡大縮小率を掛けて目的の座標を出す*/
   if (ball_d > enemy_d) {
-    x = ball_x * coefficient + radius / 2;
-    y = ball_y * coefficient + radius / 2;
+    x = unk_x * coefficient;
+    y = unk_y * coefficient;
   } else {
-    x = enemy_x * coefficient + radius / 2;
-    y = enemy_y * coefficient + radius / 2;
+    x = enemy_x * coefficient;
+    y = enemy_y * coefficient;
   }
 
   /*向きをボールの方へ*/
-  const auto theta = ai_server::util::wrap_to_2pi(std::atan2(ball_x - x, ball_y - y));
+  const auto theta = ai_server::util::wrap_to_2pi(std::atan2(dir_x - x, dir_y - y));
   /*計算した値を自機にセット*/
   ally_robot.set_position({x, y, theta});
 
