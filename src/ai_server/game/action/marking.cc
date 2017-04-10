e4f97adc7d6c9b74d9c81441010de90bc3ee6a0b
NakayamaSatoru <adlersschanze@gmail.com>
2017-04-10 20:14:11 +0900

    [clean]コードの整形
    コード整形した

--- src/ai_server/game/action/marking.cc
+++ src/ai_server/game/action/marking.cc
@@ -47,13 +47,13 @@ model::command marking::execute() {
       tmp_x = ball_x;
       tmp_y = ball_y;
       break;
-    case mark_mode::shoot_block: //シュートを阻止
-			const auto length = std::hypot( -4500 - enemy_x,enemy_y);//敵機とゴールの距離
-      ratio = (length/2 + radius_) / length;
-      x     = (1 - ratio) *  - 4500 + ratio * enemy_x;
-      y     = ratio * enemy_y;
-      tmp_x = enemy_x;
-      tmp_y = enemy_y;
+    case mark_mode::shoot_block:                                //シュートを阻止
+      const auto length = std::hypot(-4500 - enemy_x, enemy_y); //敵機とゴールの距離
+      ratio             = (length / 2 + radius_) / length;
+      x                 = (1 - ratio) * -4500 + ratio * enemy_x;
+      y                 = ratio * enemy_y;
+      tmp_x             = enemy_x;
+      tmp_y             = enemy_y;
   }
 
   //向きをボールの方へ
