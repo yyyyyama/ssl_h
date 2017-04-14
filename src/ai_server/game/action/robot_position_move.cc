301fe3cd351efc0487a17c05bbfeef1596c0a783
Sakyo <sakyo.hashimoto@gmail.com>
2017-04-14 17:49:05 +0900

    finished_ の問題を修正

--- src/ai_server/game/action/robot_position_move.cc
+++ src/ai_server/game/action/robot_position_move.cc
@@ -11,10 +11,18 @@ void robot_position_move::move_to(double x, double y, double theta) {
 }
 
 model::command robot_position_move::execute() {
-  finished_ = false;
-  model::command command(id_);
-  command.set_position({x_, y_, theta_});
-  finished_ = true;
+  const auto this_robot_team = is_yellow_ ? world_.robots_yellow() : world_.robots_blue();
+  const auto& this_robot     = this_robot_team.at(id_);
+
+  if (this_robot.x() == x_ && this_robot.y() == y_ && this_robot.theta() == theta_) {
+    //ロボットが指定位置に存在するとき
+    finished_ = true;
+  } else {
+    //ロボットが指定位置に存在しないとき
+    finished_ = false;
+    model::command command(id_);
+    command.set_position({x_, y_, theta_});
+  }
 }
 
 bool robot_position_move::finished() const {
