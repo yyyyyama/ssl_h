6e14f944176bd8ee0d56bd19a19bfb775afcb6af
Sakyo-Hashimoto <sakyo.hashimoto@gmail.com>
2017-04-15 19:08:02 +0900

    実機テストでの不具合を修正

--- src/ai_server/game/action/move.cc
+++ src/ai_server/game/action/move.cc
@@ -16,6 +16,7 @@ model::command move::execute() {
   const double theta_allow = 1.0 * M_PI / 180.0; //指定角度と取得した角度のズレの許容値[rad]
   const auto this_robot_team = is_yellow_ ? world_.robots_yellow() : world_.robots_blue();
   const auto& this_robot     = this_robot_team.at(id_);
+  model::command command(id_);
 
   if (std::abs(this_robot.x() - x_) <= xy_allow && std::abs(this_robot.y() - y_) <= xy_allow &&
       std::abs(this_robot.theta() - theta_) <= theta_allow) {
@@ -24,9 +25,10 @@ model::command move::execute() {
   } else {
     //ロボットが指定位置に存在しないとき
     finished_ = false;
-    model::command command(id_);
+    std::printf("x:%f y:%f theta:%f\n", this_robot.x(), this_robot.y(), this_robot.theta());
     command.set_position({x_, y_, theta_});
   }
+  return command;
 }
 
 bool move::finished() const {
