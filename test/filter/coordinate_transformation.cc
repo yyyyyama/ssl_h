5d819a4b15d00a8d36ad03822404431267c7060d
KondoTakumi <kontyanzhihg@gmail.com>
2017-04-18 11:24:20 +0900

    テストデータの修正

--- test/filter/coordinate_transformation.cc
+++ test/filter/coordinate_transformation.cc
@@ -18,28 +18,28 @@ BOOST_AUTO_TEST_CASE(test01, *boost::unit_test::tolerance(0.0005)) {
 
   ai_server::model::ball test_ball{100, 200, 0.0};
   ai_server::filter::coordinate_transformation<ai_server::model::ball> test_transformation1{
-      200, 100, pi<double>()};
+      200, 100, -pi<double>() / 2};
   const auto t1 = std::chrono::high_resolution_clock::time_point{};
 
   test_transformation1.apply(test_ball, t1);
 
-  BOOST_TEST(test_ball.x() == 100);
-  BOOST_TEST(test_ball.y() == -100);
+  BOOST_TEST(test_ball.x() == 400.0);
+  BOOST_TEST(test_ball.y() == 0.0);
 }
 
 BOOST_AUTO_TEST_CASE(test02, *boost::unit_test::tolerance(0.0005)) {
   using boost::math::constants::pi;
   using namespace std::chrono_literals;
 
-  ai_server::model::robot test_robot{100, 200, 0.0};
+  ai_server::model::robot test_robot{1, 100, 200, 0.0};
   ai_server::filter::coordinate_transformation<ai_server::model::robot> test_transformation2{
       200, 100, pi<double>()};
   const auto t1 = std::chrono::high_resolution_clock::time_point{};
 
   test_transformation2.apply(test_robot, t1);
 
-  BOOST_TEST(test_robot.x() == 100);
-  BOOST_TEST(test_robot.y() == -100);
+  BOOST_TEST(test_robot.x() == 100.0);
+  BOOST_TEST(test_robot.y() == -100.0);
   BOOST_TEST(test_robot.theta() == pi<double>());
 }
 
