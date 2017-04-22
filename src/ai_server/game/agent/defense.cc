02c5cc052ed38e3fdb9c74d1cff5cdc4c3989e7d
NakayamaSatoru <adlersschanze@gmail.com>
2017-04-22 23:53:46 +0900

    [add]キーパーの処理を追加
    キーパーの処理を追加したかった

--- src/ai_server/game/agent/defense.cc
+++ src/ai_server/game/agent/defense.cc
@@ -1,4 +1,7 @@
+#include <cmath>
+
 #include "ai_server/game/agent/defense.h"
+#include "ai_server/util/math.h"
 
 namespace ai_server{
 	namespace game{
@@ -9,12 +12,12 @@ namespace ai_server{
 			std::vector<std::shared_ptr<action::base>> defens::execute(){
 
 			  	//キーパー用のaction
-				  action::move keeper(keeper_id_);
+				  auto keeper = std::make_shared<action::move>(keeper_id_);
 					
 				  //壁用のaction
-					std::vector<std::shared_ptr<action::base>> wall;
+					std::vector<std::shared_ptr<action::move>> wall;
 					for(auto it = ids_.begin() ; it!=ids_.end() ; ++it){
-						wall.push_back(std::make_shared<action::base>(*it));
+						wall.push_back(std::make_shared<action::move>(*it));
 					}
 					
 					//ボールの座標
@@ -42,8 +45,10 @@ namespace ai_server{
 					//基準点とボールを結んだ直線と垂直に交わる直線の切片
 					const auto segment = y_ - inclination*x_;
 					//基準点からボールへの向き
-					const auto theta = util::wrap_to_2pi(std::atan2(y - tmp_y, x - tmp_x) + pi<double>());
-
+					const auto theta = util::wrap_to_2pi(std::atan2(ball_y - y_, ball_x - x_) + pi<double>());
+	
+					//ここから壁の処理
+					//
 					//壁のイテレータ
 					auto wall_it = wall.begin();
 					//基準点からどれだけずらすか
@@ -67,17 +72,51 @@ namespace ai_server{
 
 					//shift_real : 実際に足したり引いたりされるずらし具合
 					for(auto shift_real = shift; wall_it != wall.end(); shift_real*-1 , ++wall_it){
-						if(shift_real<0){
-							tmp = length / (length + shift_real); //敵位置 - 自位置の比
+
+						if(std::signbit(shift_real)){//基準点より左側
+							tmp = length / (length + shift_real); 
       				ratio = 1 - tmp;
 							(*wall_it->move_to((-ratio * tmp_x + x_) / tmp , (-ratio * tmp_y + y_) / tmp ,theta);
 							shift_real-=shift;
-						}else{
+						}else{//基準点より右側
 							ratio = (shift_real) / length;//全体に対してのずらし具合の比
       				(*wall_it)->move_to( (1 - ratio) * x_ + ratio * tmp_x , (1 - ratio) * y_ + ratio * tmp_y , theta );//置く場所をセット
 						}
 					}
       		
+					//ここからキーパーの処理
+					//縄張りの大きさ
+					const auto demarcation = 2250.0;
+					
+					if(std::signbit(ball_x) == std::signbit(goal_x*(-1)) ){//ボールは敵陣地なのでキーパーはさがる:A
+						//ゴール直前でボールに併せて横移動
+						
+						keeper->move(goal_x , ball_y , util::wrap_to_2pi(std::atan2(0,goal_x-ball_x) + pi<double>()));
+					
+					}else if(std::signbit(std::pow(ball_x-world_.field().x_max(),2)+std::pow(ball_y,2)-std::pow(demarcation,2))){//ボールは縄張りのなかなので頑張れキーパー:C
+						//ゴール前で張ってるキーパの位置
+						length = std::hypot(x_ - ball_x, y_ - ball_y); //基準点<->ボール
+						ratio = (180) / length;//全体に対してのキーパー位置の比
+      			
+						keeper->move_to( (1 - ratio) * x_ + ratio * tmp_x , (1 - ratio) * y_ + ratio * tmp_y , theta );//置く場所をセット
+						
+						//もし支線の先にロボットがいたら
+						//そのロボットの視線の先に移動
+						//セット仕直し
+					}else{//ボールはこっち陣地なので壁の補強:B
+						//壁のすぐ後ろで待機
+						
+						//基準点からちょっと下がったキーパの位置
+						length = std::hypot(x_ - ball_x, y_ - ball_y); //基準点<->ボール
+						ratio = (800) / length;//全体に対してのキーパー位置の比
+      			
+						keeper->move_to( (1 - ratio) * x_ + ratio * tmp_x , (1 - ratio) * y_ + ratio * tmp_y , theta );//置く場所をセット
+					
+					}
+
+					wall.push_back(keeper);//配列を返すためにキーパーも統合する
+
+					return wall;//返す
 			}
 
 		}
