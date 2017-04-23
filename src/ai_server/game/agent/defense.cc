d10d8067b47ea07f689f4cb5c8530f7d6c8bb8ae
NakayamaSatoru <adlersschanze@gmail.com>
2017-04-23 11:06:04 +0900

    [fix]いろいろなバグを直した
    ビルドが取り敢えずとおる様にしたかった

--- src/ai_server/game/agent/defense.cc
+++ src/ai_server/game/agent/defense.cc
@@ -1,124 +1,134 @@
 #include <cmath>
+#include <iostream>
 
 #include "ai_server/game/agent/defense.h"
+#include "ai_server/game/action/move.h"
 #include "ai_server/util/math.h"
 
-namespace ai_server{
-	namespace game{
-		namespace action{
-			
-			defense::defense(const model::world& world,bool is_yellow,unsigned int keeper_id,const std::vector<unsigned int>& ids):keeper_id_(keeper_id),ids_(ids){}
+namespace ai_server {
+namespace game {
+namespace agent {
 
-			std::vector<std::shared_ptr<action::base>> defens::execute(){
+defense::defense(const model::world& world, bool is_yellow, unsigned int keeper_id,
+                 const std::vector<unsigned int>& wall_ids)
+    : base(world, is_yellow), keeper_id_(keeper_id), wall_ids_(wall_ids) {}
 
-			  	//キーパー用のaction
-				  auto keeper = std::make_shared<action::move>(keeper_id_);
-					
-				  //壁用のaction
-					std::vector<std::shared_ptr<action::move>> wall;
-					for(auto it = ids_.begin() ; it!=ids_.end() ; ++it){
-						wall.push_back(std::make_shared<action::move>(*it));
-					}
-					
-					//ボールの座標
-					const auto ball_x = world_.ball().x();
-					const auto ball_y = world_.ball().y();
-					//ゴールの座標
-					const auto goal_x = -1 * world_.field().x_max();
+std::vector<std::shared_ptr<action::base>> defense::execute() {
+  using boost::math::constants::pi;
 
-					//比
-					auto ratio = 0.0;
-					auto length = 0.0;
-					//適当な計算用
-					auto tmp = 0.0;
+  //キーパー用のaction
+  std::shared_ptr<action::base> keeper =
+      std::make_shared<action::move>(world_, is_yellow_, keeper_id_);
 
-					//各ロボットの基準点.今回はボールとゴールの直線とボールエリアの境界線の交点
-					length = std::hypot(goal_x - ball_x, ball_y); //ボールとゴールの距離
+  //壁用のaction
+  std::vector<std::shared_ptr<action::base>> wall;
+  for (auto it = wall_ids_.begin(); it != wall_ids_.end(); ++it) {
+    wall.push_back(std::make_shared<action::move>(world_, is_yellow_, *it));
+  }
 
-      		ratio = (1500.0) / length;//全体に対してのゴールエリアの大きさの比
-					//基準座標
-      		x_     = (1 - ratio) * goal_x + ratio * ball_x;
-      		y_     = ratio * ball_y;
-				
-					//基準点とボールを結んだ直線と垂直に交わる直線の傾き
-					const auto inclination = -((ball_y - y_)/(ball_x - x_));
-					//基準点とボールを結んだ直線と垂直に交わる直線の切片
-					const auto segment = y_ - inclination*x_;
-					//基準点からボールへの向き
-					const auto theta = util::wrap_to_2pi(std::atan2(ball_y - y_, ball_x - x_) + pi<double>());
-	
-					//ここから壁の処理
-					//
-					//壁のイテレータ
-					auto wall_it = wall.begin();
-					//基準点からどれだけずらすか
-					auto shift = 0.0;
+  //ボールの座標
+  const auto ball_x = world_.ball().x();
+  const auto ball_y = world_.ball().y();
+  //ゴールの座標
+  const auto goal_x = -1 * world_.field().x_max();
 
-					//壁の数が偶数奇数の判定
-					if(ids_.size()%2 == 0){//偶数
-						(*wall_it)->move_to(x_,y_,theta);
-						++wall_it;
-						shift = 180.0;
-					}else{
-						shift = 90.0;
-					}
+  //比
+  auto ratio  = 0.0;
+  auto length = 0.0;
+  //適当な計算用
+  auto tmp = 0.0;
 
-					//直線を引くための適当な点
-					const auto tmp_x = 1000.0;
-					const auto tmp_y = inclination*tmp_x + segment;
-					
-					//基準点から左右に配置するロボットの座標
-					length = std::hypot(x_ - tmp_x, y_ - tmp_y); //基準点<->適当な点の長さ
+  //各ロボットの基準点.今回はボールとゴールの直線とボールエリアの境界線の交点
+  length = std::hypot(goal_x - ball_x, ball_y); //ボールとゴールの距離
 
-					//shift_real : 実際に足したり引いたりされるずらし具合
-					for(auto shift_real = shift; wall_it != wall.end(); shift_real*-1 , ++wall_it){
+  ratio = (1500.0) / length; //全体に対してのゴールエリアの大きさの比
+                             //基準座標
+  x_ = (1 - ratio) * goal_x + ratio * ball_x;
+  y_ = ratio * ball_y;
 
-						if(std::signbit(shift_real)){//基準点より左側
-							tmp = length / (length + shift_real); 
-      				ratio = 1 - tmp;
-							(*wall_it->move_to((-ratio * tmp_x + x_) / tmp , (-ratio * tmp_y + y_) / tmp ,theta);
-							shift_real-=shift;
-						}else{//基準点より右側
-							ratio = (shift_real) / length;//全体に対してのずらし具合の比
-      				(*wall_it)->move_to( (1 - ratio) * x_ + ratio * tmp_x , (1 - ratio) * y_ + ratio * tmp_y , theta );//置く場所をセット
-						}
-					}
-      		
-					//ここからキーパーの処理
-					//縄張りの大きさ
-					const auto demarcation = 2250.0;
-					
-					if(std::signbit(ball_x) == std::signbit(goal_x*(-1)) ){//ボールは敵陣地なのでキーパーはさがる:A
-						//ゴール直前でボールに併せて横移動
-						
-						keeper->move(goal_x , ball_y , util::wrap_to_2pi(std::atan2(0,goal_x-ball_x) + pi<double>()));
-					
-					}else if(std::signbit(std::pow(ball_x-world_.field().x_max(),2)+std::pow(ball_y,2)-std::pow(demarcation,2))){//ボールは縄張りのなかなので頑張れキーパー:C
-						//ゴール前で張ってるキーパの位置
-						length = std::hypot(x_ - ball_x, y_ - ball_y); //基準点<->ボール
-						ratio = (180) / length;//全体に対してのキーパー位置の比
-      			
-						keeper->move_to( (1 - ratio) * x_ + ratio * tmp_x , (1 - ratio) * y_ + ratio * tmp_y , theta );//置く場所をセット
-						
-						//もし支線の先にロボットがいたら
-						//そのロボットの視線の先に移動
-						//セット仕直し
-					}else{//ボールはこっち陣地なので壁の補強:B
-						//壁のすぐ後ろで待機
-						
-						//基準点からちょっと下がったキーパの位置
-						length = std::hypot(x_ - ball_x, y_ - ball_y); //基準点<->ボール
-						ratio = (800) / length;//全体に対してのキーパー位置の比
-      			
-						keeper->move_to( (1 - ratio) * x_ + ratio * tmp_x , (1 - ratio) * y_ + ratio * tmp_y , theta );//置く場所をセット
-					
-					}
+  //基準点とボールを結んだ直線と垂直に交わる直線の傾き
+  const auto inclination = -((ball_y - y_) / (ball_x - x_));
+  //基準点とボールを結んだ直線と垂直に交わる直線の切片
+  const auto segment = y_ - inclination * x_;
+  //基準点からボールへの向き
+  const auto theta = util::wrap_to_2pi(std::atan2(ball_y - y_, ball_x - x_) + pi<double>());
 
-					wall.push_back(keeper);//配列を返すためにキーパーも統合する
+  //ここから壁の処理
+  //
+  //壁のイテレータ
+  auto wall_it = wall.begin();
+  //基準点からどれだけずらすか
+  auto shift = 0.0;
 
-					return wall;//返す
-			}
+  //壁の数が偶数奇数の判定
+  if (wall_ids_.size() % 2 == 0) { //偶数
+		std::dynamic_pointer_cast<action::move>(*wall_it)->move_to(x_, y_, theta);
+    ++wall_it;
+    shift = 180.0;
+  } else {
+    shift = 90.0;
+  }
 
-		}
-	}
+  //直線を引くための適当な点
+  const auto tmp_x = 1000.0;
+  const auto tmp_y = inclination * tmp_x + segment;
+
+  //基準点から左右に配置するロボットの座標
+  length = std::hypot(x_ - tmp_x, y_ - tmp_y); //基準点<->適当な点の長さ
+
+  // shift_real : 実際に足したり引いたりされるずらし具合
+  for (auto shift_real = shift; wall_it != wall.end(); shift_real *= -1, ++wall_it) {
+    if (std::signbit(shift_real)) { //基準点より左側
+      tmp   = length / (length + shift_real);
+      ratio = 1 - tmp;
+      std::dynamic_pointer_cast<action::move>(*wall_it)->move_to((-ratio * tmp_x + x_) / tmp, (-ratio * tmp_y + y_) / tmp, theta);
+      shift_real -= shift;
+    } else {                         //基準点より右側
+      ratio = (shift_real) / length; //全体に対してのずらし具合の比
+      std::dynamic_pointer_cast<action::move>(*wall_it)->move_to((1 - ratio) * x_ + ratio * tmp_x, (1 - ratio) * y_ + ratio * tmp_y,
+                          theta); //置く場所をセット
+    }
+  }
+
+  //ここからキーパーの処理
+  //縄張りの大きさ
+  const auto demarcation = 2250.0;
+
+  if (std::signbit(ball_x) ==
+      std::signbit(goal_x * (-1))) { //ボールは敵陣地なのでキーパーはさがる:A
+    //ゴール直前でボールに併せて横移動
+
+    std::dynamic_pointer_cast<action::move>(keeper)->move_to(goal_x, ball_y,
+                    util::wrap_to_2pi(std::atan2(0, goal_x - ball_x) + pi<double>()));
+
+  } else if (std::signbit(
+                 std::pow(ball_x - world_.field().x_max(), 2) + std::pow(ball_y, 2) -
+                 std::pow(demarcation, 2))) { //ボールは縄張りのなかなので頑張れキーパー:C
+    //ゴール前で張ってるキーパの位置
+    length = std::hypot(x_ - ball_x, y_ - ball_y); //基準点<->ボール
+    ratio  = (180) / length; //全体に対してのキーパー位置の比
+
+    std::dynamic_pointer_cast<action::move>(keeper)->move_to((1 - ratio) * x_ + ratio * tmp_x, (1 - ratio) * y_ + ratio * tmp_y,
+                    theta); //置く場所をセット
+
+    //もし支線の先にロボットがいたら
+    //そのロボットの視線の先に移動
+    //セット仕直し
+  } else { //ボールはこっち陣地なので壁の補強:B
+    //壁のすぐ後ろで待機
+
+    //基準点からちょっと下がったキーパの位置
+    length = std::hypot(x_ - ball_x, y_ - ball_y); //基準点<->ボール
+    ratio  = (800) / length; //全体に対してのキーパー位置の比
+
+    std::dynamic_pointer_cast<action::move>(keeper)->move_to((1 - ratio) * x_ + ratio * tmp_x, (1 - ratio) * y_ + ratio * tmp_y,
+                    theta); //置く場所をセット
+  }
+
+  wall.push_back(keeper); //配列を返すためにキーパーも統合する
+
+  return wall; //返す
+}
+}
+}
 }
--- src/ai_server/game/agent/defense.h
+++ src/ai_server/game/agent/defense.h
@@ -6,21 +6,22 @@
 
 #include "ai_server/game/agent/base.h"
 
-namespace ai_server{
-	namespace game{
-		namespace action{
-			class defense:public base{
-				public:
-				defense(const model::world& world,bool is_yellow,unsigned int keeper_id,const std::vector<unsigned int>& ids);
-				std::vector<std::shared_ptr<action::base>> execute() override;
-				private:
-					std::vector<unsigned int>& ids_;
-					unsigned int keeper_id_;
-					double x_=0;
-					double y_=0;
-			}
+namespace ai_server {
+namespace game {
+namespace agent {
+class defense : public base {
+public:
+  defense(const model::world& world, bool is_yellow, unsigned int keeper_id,
+          const std::vector<unsigned int>& wall_ids);
+  std::vector<std::shared_ptr<action::base>> execute() override;
 
-		}
-	}
+private:
+  unsigned int keeper_id_;
+  const std::vector<unsigned int>& wall_ids_;
+  double x_ = 0.0;
+  double y_ = 0.0;
+};
 }
-#endif//AI_SERVER_GAME_ACTION_DEFENSE_H
+}
+}
+#endif // AI_SERVER_GAME_ACTION_DEFENSE_H
