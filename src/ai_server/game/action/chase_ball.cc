fe5e03309520e9567703c264c083e01a2879ba2c
ken <ken@localhost.localdomain>
2017-04-23 18:50:05 +0900

    add comment and change

--- src/ai_server/game/action/chase_ball.cc
+++ src/ai_server/game/action/chase_ball.cc
@@ -12,6 +12,7 @@ model::command chase_ball::execute() {
 
 	using namespace Eigen;
 
+	//ボールデータ
 	const auto ball_x = world_.ball().x();
 	const auto ball_y = world_.ball().y();
 
@@ -20,25 +21,31 @@ model::command chase_ball::execute() {
 
 	my_pos = {robot.x(), robot.y(), robot.theta()};
 
+	//自分->ボール
 	Vector2d v1(ball_x - my_pos.x, ball_y - my_pos.y);
 	v1.normalized();
 
-	second_pos = {ball_x - r * v1(0), ball_y - r * v1(1), std::atan2(v1(0),v1(1))};
+	first_pos = {ball_x - r * v1(0), ball_y - r * v1(1), std::atan2(v1(0),v1(1))};
 
+	//ボール->ターゲット
 	Vector2d v2(target_x - ball_x, target_y - ball_y); 
 	v2.normalized();
 	
-	final_pos = {ball_x - r * v2(0), ball_y - r * v2(1), std::atan2(v2(0),v2(1))};
+	second_pos = {ball_x - r * v2(0), ball_y - r * v2(1), std::atan2(v2(0),v2(1))};
 
-	auto dist1 = std::hypot(my_pos.x - second_pos.x, my_pos.y - second_pos.y);
-	auto dist2 = std::hypot(my_pos.x - final_pos.x, my_pos.y - final_pos.y);
+	//それぞれの距離
+	auto dist1 = std::hypot(my_pos.x - first_pos.x, my_pos.y - first_pos.y);
+	auto dist2 = std::hypot(my_pos.x - second_pos.x, my_pos.y - second_pos.y);
 
+	//円周上を回る角度
 	auto move_angle = util::wrap_to_pi(v2(2) - v1(2));
 
+	//初期設定
 	if (phase == 0) {
 		target_x = 4500.;
 		target_y = 0.;
 
+		//first_posまでの 距離が長いとき->select = 1, 距離が短いとき->select = 2
 		if (dist1 >= acc_dist * 2) {
 			p1_count = acc_count * 2 + (dist1 - acc_dist * 2) / max_vel;
 			select = 1;
@@ -49,49 +56,56 @@ model::command chase_ball::execute() {
 		}
 
 		p2_count = std::abs(move_angle) / (rot_omega / 60.);
-
 		phase = 1;
 	}
 
+	//first_posに移動
 	if (phase == 1) {
 		count++;
-		auto omega1 = ( util::wrap_to_pi(second_pos.theta - my_pos.theta) ) / (p1_count - count);
+		auto omega1 = ( util::wrap_to_pi(first_pos.theta - my_pos.theta) ) / (p1_count - count);
 
-		switch (select) {
-			case 1 : if (count <= acc_count) {
-									next_vel = {acc * count / 60. * std::cos(v1(2)), acc * count / 60. * std::sin(v1(2)), omega1};
-								}
-								else if (count > acc_count && dist1 >= acc_dist) {
+			//距離が長いとき
+			if (select == 1) {
+				if (count <= acc_count) {
+					next_vel = {acc * count / 60. * std::cos(v1(2)), acc * count / 60. * std::sin(v1(2)), omega1};
+				}
+
+				else if (count > acc_count && dist1 >= acc_dist) {
 									next_vel = {max_vel * std::cos(v1(2)), max_vel * std::sin(v1(2)), omega1};
-								}
-								else if (dist1 < acc_dist) {
-									sub_count++;
-									if(sub_count <= acc_count) {
-										next_vel = {acc * (acc_count - sub_count) / 60. * std::cos(v1(2)), acc * (acc_count - sub_count) / 60. * std::sin(v1(2)), omega1};
-									}
-									if(dist1 < 100) {
-										phase = 2;
-									}
-								}
-								break;
+				}
 
-			case 2 : if (count <= p1_count / 2) {
-									next_vel = {acc * count / 60. * std::cos(v1(2)), acc * count / 60. * std::sin(v1(2)), omega1};
-								}
-								else {
-									sub_count++;
-									if (sub_count < p1_count) {
-										next_vel = {acc * (p1_count / 2. - sub_count) / 60. * std::cos(v1(2)), acc * (p1_count / 2. - sub_count) / 60. * std::sin(v1(2)), omega1};
-									}
-									if(dist1 < 100) {
-										count = 0;
-										sub_count = 0;
-										phase = 2;
-									}
-								}
-								break;
-		}
-	} 
+				else if (dist1 < acc_dist) {
+					sub_count++;
+					if (sub_count <= acc_count) {
+							next_vel = {acc * (acc_count - sub_count) / 60. * std::cos(v1(2)), acc * (acc_count - sub_count) / 60. * std::sin(v1(2)), omega1};
+					}
+					if (dist1 < 100) {
+						phase = 2;
+					}
+					else {
+						next_vel = {(first_pos.x - my_pos.x) / 10., (first_pos.y - my_pos.y) / 10., (util::wrap_to_pi(first_pos.theta - my_pos.theta)) / 10.};
+					}
+				}
+			}
+
+			//距離が短いとき
+			else if (select == 2) {
+				if (count <= p1_count / 2) {
+					next_vel = {acc * count / 60. * std::cos(v1(2)), acc * count / 60. * std::sin(v1(2)), omega1};
+				}
+				else {
+					sub_count++;
+					if (sub_count < p1_count) {
+						next_vel = {acc * (p1_count / 2. - sub_count) / 60. * std::cos(v1(2)), acc * (p1_count / 2. - sub_count) / 60. * std::sin(v1(2)), omega1};
+					}
+					if(dist1 < 100) {
+						count = 0;
+						sub_count = 0;
+						phase = 2;
+					}
+				}
+			}
+	}
 
 	else if (phase == 2){
 		count++;
@@ -112,13 +126,15 @@ model::command chase_ball::execute() {
 			phase = 3;
 		}
 	}
-	
+
+	//ドリブルしながら直進
 	else if (phase == 3) {
 		next_vel = {300 * std::cos(v1(2)), 300 * std::sin(v1(2)), 0};
 		command.set_dribble(5);
 		fin_flag = true;
 	}
 
+	//ボールの速度を加算
 	next_vel = {next_vel.vx + ball_vx, next_vel.vy + ball_vy, next_vel.omega};
 	command.set_velocity(next_vel);
 
--- src/ai_server/game/action/chase_ball.h
+++ src/ai_server/game/action/chase_ball.h
@@ -18,33 +18,33 @@ public:
 	bool finished() const override;
 	
 private:
-	model::command::position_t first_pos;
-	model::command::position_t second_pos;
-	model::command::position_t final_pos;
-	model::command::position_t my_pos;
+	model::command::position_t first_pos;			//ボールからrの円周上
+	model::command::position_t second_pos;		//最終位置
+	model::command::position_t my_pos;				//今の位置
 
-	model::command::velocity_t next_vel;
+	model::command::velocity_t next_vel;			//次の速度
 
 	int phase = 0;
 	int select = 0;
 	int count = 0;
 	int sub_count = 0;
 
-	double target_x = 0;
+	double target_x = 0;		//ターゲット
 	double target_y = 0;
 
-	int p1_count = 0;
-	int p2_count = 0;
+	int p1_count = 0;		//phase1にかかる時間
+	int p2_count = 0;		//phase2にかかる時間
 
-	const double r = 300;
+	const double r = 300;			//ボールに対する半径
 	
-	const double acc = 3000;
-	const int acc_count = 60;
-	const double acc_dist = acc * pow(acc_count / 60., 2) / 2;
-	const double max_vel = acc * acc_count / 60.;
+	const double acc = 3000;		//加速度
+	const int acc_count = 60;	//加速時間
+	const double acc_dist = acc * pow(acc_count / 60., 2) / 2;	//加速にかかる距離
+	const double max_vel = acc * acc_count / 60.;	//最高速度
 
-	const double rot_omega = 180 * pi<double>() / 180.;
-	const double rot_vel = r * rot_omega;
+	const double rot_omega = 180 * pi<double>() / 180.;		//円周上を回るときの角速度
+	const double rot_vel = r * rot_omega;		//円周上を回るときの速度
+
 	
 	bool fin_flag = false;
 };
