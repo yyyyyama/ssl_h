3801bc98a2bed05c77e4f0567b7c5efc8816375f
Toshiki Mimura <toshiki@localhost.localdomain>
2017-04-25 00:29:37 +0900

    boost/math/constants/constants.hの削除
    過去の名残で残っていた不必要な箇所
    当然,削除

--- src/ai_server/filter/acceleration_calculator.cc
+++ src/ai_server/filter/acceleration_calculator.cc
@@ -1,5 +1,4 @@
 #include "acceleration_calculator.h"
-#include <boost/math/constants/constants.hpp>
 
 namespace ai_server {
 namespace filter {
@@ -7,7 +6,6 @@ namespace filter {
 template <>
 void acceleration_calculator<model::robot>::apply(
     model::robot& robot, std::chrono::high_resolution_clock::time_point time) {
-  using namespace boost::math::constants;
   const auto elapsed_time = std::chrono::duration<double>(time - prev_time_);
   robot.set_ax((robot.vx() - prev_state_.vx()) / elapsed_time.count());
   robot.set_ay((robot.vy() - prev_state_.vy()) / elapsed_time.count());
