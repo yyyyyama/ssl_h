ef8da0a296c0e47a281b713740ba39429f8bfc8b
NakayamaSatoru <adlersschanze@gmail.com>
2017-04-25 18:13:03 +0900

    [clean]コメントを付けるなどして読みやすくした
    作業途中とはいえ,クッソみにくかったので掃除をした

--- src/ai_server/game/agent/defense.cc
+++ src/ai_server/game/agent/defense.cc
@@ -16,6 +16,11 @@ defense::defense(const model::world& world, bool is_yellow, unsigned int keeper_
 std::vector<std::shared_ptr<action::base>> defense::execute() {
   using boost::math::constants::pi;
 
+  // actionの生成部分
+  //
+  // actionの初期化は一回しか行わない
+  //
+  //
   if (flag_) {
     //キーパー用のaction
     keeper_ = std::make_shared<action::move>(world_, is_yellow_, keeper_id_);
@@ -26,13 +31,10 @@ std::vector<std::shared_ptr<action::base>> defense::execute() {
     }
   }
 
-  const auto myrobot = is_yellow_ ? world_.robots_yellow() : world_.robots_blue();
-  const auto nyan    = myrobot.at(keeper_id_);
-  std::cout << "nyyyyyyyyyyan" << nyan.x() << "    " << nyan.y() << std::endl;
-
   //ボールの座標
   const auto ball_x = world_.ball().x();
   const auto ball_y = world_.ball().y();
+
   //ゴールの座標
   const auto goal_x =
       (world_.field().x_max() + (std::signbit(world_.field().x_max()) ? 200.0 : -200.0));
@@ -46,28 +48,43 @@ std::vector<std::shared_ptr<action::base>> defense::execute() {
   //適当な計算用
   auto tmp = 0.0;
 
-  //各ロボットの基準点.今回はボールとゴールの直線とボールエリアの境界線の交点
-  length = std::hypot(goal_x - ball_x, ball_y); //ボールとゴールの距離
+  //基準座標を求めている
+  //
+  //いい感じのところ
+  //
+  //
+  length = std::hypot(goal_x - ball_x, ball_y); //ボール<->ゴール
 
-  ratio = (1250.0) / length; //全体に対してのゴールエリアの大きさの比
-                             //基準座標
+  ratio = (1250.0) / length; //全体に対しての基準座標の比
+
   x_ = (1 - ratio) * goal_x + ratio * ball_x;
   y_ = ratio * ball_y;
-  if (y_ >= -250 && y_ <= 250) {
+
+  if (y_ >= -250 && y_ <= 250) { //もし基準座標が直線の範囲だったら直線に叩き込む
     x_ = (goal_x + (std::signbit(goal_x) ? 1000 : -1000));
   }
+
   std::cout << "x_:" << x_ << " y_:" << y_ << std::endl;
+
   //基準点とボールを結んだ直線と垂直に交わる直線の傾き
   const auto inclination = -1 / ((ball_y - y_) / (ball_x - x_));
+
   //基準点とボールを結んだ直線と垂直に交わる直線の切片
   const auto segment = y_ - inclination * x_;
+
   //基準点からボールへの向き
   const auto theta = util::wrap_to_2pi(std::atan2(ball_y - y_, ball_x - x_));
 
   //ここから壁の処理
+  //
+  //
+  //
+  //
+  //
 
   //壁のイテレータ
   auto wall_it = wall_.begin();
+
   //基準点からどれだけずらすか
   auto shift = 0.0;
 
@@ -90,23 +107,29 @@ std::vector<std::shared_ptr<action::base>> defense::execute() {
   }
 
   //ここからキーパーの処理
-  //縄張りの大きさ
-  const auto demarcation = 2250.0;
+  //
+  //キーパーはボールの位置によって動き方が3種類ある.
+  //
+  // A:ボールが敵陣地なので多分そこまで動く必要はない
+  // B:ボールが自陣地なので壁の補強をしなければ
+  // C:ボールはゴールの直ぐ目の前なのでゴールまえでジャンプしてでも止める
+  //
+  //
+  const auto demarcation = 2250.0; //縄張りの大きさ
 
-  if (std::signbit(ball_x) ==
-      std::signbit(goal_x * (-1))) { //ボールは敵陣地なのでキーパーはさがる:A
+  if (std::signbit(ball_x) == std::signbit(goal_x * (-1))) { // A
     //ゴール直前でボールに併せて横移動
 
     auto keeper_y = ((ball_y >= -500 && ball_y <= 500) ? ball_y : 0);
     keeper_->move_to(goal_x, keeper_y,
                      util::wrap_to_2pi(std::atan2(0, goal_x - ball_x) + pi<double>()));
 
-    // keeper_->move_to(0,0,0);
-  } else if (std::signbit(
-                 std::pow(ball_x - world_.field().x_max(), 2) + std::pow(ball_y, 2) -
-                 std::pow(demarcation, 2))) { //ボールは縄張りのなかなので頑張れキーパー:C
+  } else if (std::signbit(std::pow(ball_x - world_.field().x_max(), 2) + std::pow(ball_y, 2) -
+                          std::pow(demarcation, 2))) { // C
+    //ゴール前でディフェンスする
+
     //ゴール前で張ってるキーパの位置
-    length = std::hypot(goal_x - ball_x, ball_y); //基準点<->ボール
+    length = std::hypot(goal_x - ball_x, ball_y); //ゴール<->ボール
     ratio  = (400) / length; //全体に対してのキーパー位置の比
 
     keeper_->move_to((1 - ratio) * goal_x + ratio * ball_x, ratio * ball_y,
@@ -115,26 +138,26 @@ std::vector<std::shared_ptr<action::base>> defense::execute() {
     //もし支線の先にロボットがいたら
     //そのロボットの視線の先に移動
     //セット仕直し
-  } else { //ボールはこっち陣地なので壁の補強:B
+  } else { // B
     //壁のすぐ後ろで待機
 
     //基準点からちょっと下がったキーパの位置
     length = std::hypot(goal_x - ball_x, ball_y); //基準点<->ボール
     ratio  = (800) / length; //全体に対してのキーパー位置の比
 
-    auto tes_x = (1 - ratio) * goal_x + ratio * ball_x;
-    auto tes_y = ratio * ball_y;
-    std::cout << "tes_x : " << tes_x << "tes_y : " << tes_y << std::endl;
-
-    keeper_->move_to(tes_x, tes_y, theta); //置く場所をセット
+    keeper_->move_to((1 - ratio) * goal_x + ratio * ball_x, ratio * ball_y,
+                     theta); //置く場所をセット
   }
 
-  if (flag_ != true) {
+  if (flag_ != true) { //もし一回目のループではなかったら前回のキーパーを削除
     wall_.pop_back();
   }
-  flag_ = false;
-  wall_.push_back(keeper_); //配列を返すためにキーパーも統合する
-  std::vector<std::shared_ptr<action::base>> re_wall{wall_.begin(), wall_.end()};
+  flag_ = false; //一回でも呼ばれたらfalseにする
+
+  wall_.push_back(keeper_); //配列を返すためにキーパーを統合する
+  std::vector<std::shared_ptr<action::base>> re_wall{
+      wall_.begin(), wall_.end()}; //型を合わせるために無理矢理作り直す
+
   return re_wall; //返す
 }
 }
