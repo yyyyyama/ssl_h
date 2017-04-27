3d1ff20a9be96c91a35251cb00a1e6d653f986b3
Kenta Sato <tosainu.maple@gmail.com>
2017-04-27 18:46:42 +0900

    Merge pull request #102 from ToshikiMimura/issue/96-sliding_mode_control_for_PP
    issue/96 sliding mode control for PP

---
+++ src/ai_server/controller/detail/sliding_mode_controller.cc
@@ -0,0 +1,46 @@
+#include <boost/math/special_functions/sign.hpp>
+#include <cmath>
+
+#include "sliding_mode_controller.h"
+
+namespace ai_server {
+namespace controller {
+namespace detail {
+
+const double sliding_mode_controller::a_max_ = 2000.0;
+const double sliding_mode_controller::kp_    = 1.0;
+
+sliding_mode_controller::sliding_mode_controller(double cycle) : cycle_(cycle) {
+  v_target_ = 0.0;
+}
+
+double sliding_mode_controller::control(const double delta_p) {
+  double a_required = std::abs(delta_p) * std::pow(kp_, 2);
+  double state;
+
+  // 必要な収束加速度が最大加速度を上回り,bangbang制御が必要か
+  if (a_required < a_max_) {
+    // 普通のSlidingMode
+    state = kp_ * delta_p + v_target_;
+  } else {
+    // bangbang制御をするための特別なstate関数
+    // 極度に非線形なため正か負かでstate切り替え
+    int sgn   = boost::math::sign(delta_p);
+    double pm = sgn * a_max_ / std::pow(kp_, 2);
+    double vm = -sgn * a_max_ / kp_;
+    state     = sgn * std::sqrt(sgn * (delta_p - pm)) + (v_target_ - vm);
+  }
+
+  if (std::abs(state) < a_max_ * cycle_) {
+    v_target_ = -kp_ * delta_p; // stateが0になるように速度を保つ
+  } else if (state < 0) {       // stateが-なら加速
+    v_target_ += a_max_ * cycle_;
+  } else { // stateが+なら減速
+    v_target_ -= a_max_ * cycle_;
+  }
+  return v_target_;
+}
+
+} // detail
+} // controller
+} // ai_server
