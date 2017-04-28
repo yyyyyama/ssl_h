333fc75eba404afe0f3ca3055b2cc0f09fb33bee
hatokani2 <atsuya887@gmail.com>
2017-04-28 19:26:44 +0900

    フォーマットを修正

--- src/ai_server/game/agent/penalty_kick.h
+++ src/ai_server/game/agent/penalty_kick.h
@@ -11,37 +11,37 @@
 #include <vector>
 #include <boost/variant.hpp>
 
-namespace ai_server{
-namespace game{
-namespace agent{
+namespace ai_server {
+namespace game {
+namespace agent {
 
-class penalty_kick : public base{
+class penalty_kick : public base {
 public:
-    penalty_kick(const model::world& world, bool is_yellow, unsigned int kicker_id, const std::vector<unsigned int>& ids);
-    bool start_flag() const;
-    void set_start_flag(bool start_flag);
-    std::vector<std::shared_ptr<action::base>> execute() override;
+  penalty_kick(const model::world& world, bool is_yellow, unsigned int kicker_id,
+               const std::vector<unsigned int>& ids);
+  bool start_flag() const;
+  void set_start_flag(bool start_flag);
+  std::vector<std::shared_ptr<action::base>> execute() override;
 
 private:
-    //パラメータ計算
-    void calculate();
+  //パラメータ計算
+  void calculate();
 
-    bool start_flag_;
-    std::vector<std::shared_ptr<action::base>> exe;
-    std::shared_ptr<action::move> move;
-    std::shared_ptr<action::kick_action> kick;
-    
-    unsigned int kicker_id_;
-    //PK待機位置
-    double kick_x;
-    double kick_y;
-    double kick_theta;
+  bool start_flag_;
+  std::vector<std::shared_ptr<action::base>> exe;
+  std::shared_ptr<action::move> move;
+  std::shared_ptr<action::kick_action> kick;
 
-    //蹴る方向
-    double target_x;
-    double target_y;
+  unsigned int kicker_id_;
+  // PK待機位置
+  double kick_x;
+  double kick_y;
+  double kick_theta;
+
+  //蹴る方向
+  double target_x;
+  double target_y;
 };
-
 }
 }
 }
