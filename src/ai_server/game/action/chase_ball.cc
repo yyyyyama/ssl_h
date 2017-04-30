d96dcbe63f2addd921cff7c20d00951279e9dc59
ken <ken@localhost.localdomain>
2017-05-01 01:20:44 +0900

    changed

--- src/ai_server/game/action/chase_ball.cc
+++ src/ai_server/game/action/chase_ball.cc
@@ -1,148 +1,177 @@
 #include <Eigen/Core>
+#include <cmath>
 #include "ai_server/game/action/chase_ball.h"
+#include "ai_server/util/math.h"
 
 namespace ai_server {
 namespace game {
 namespace action {
 
+void chase_ball::set_target(double x, double y) {
+  target_x_ = x;
+  target_y_ = y;
+}
+
 model::command chase_ball::execute() {
-	model::command command(id_);
-	const auto fri_robots = is_yellow_ ? world_.robots_yellow() : world_.robots_blue();
-	const auto& robot = fri_robots.at(id_);
+  model::command command(id_);
+  const auto fri_robots = is_yellow_ ? world_.robots_yellow() : world_.robots_blue();
+  const auto& robot     = fri_robots.at(id_);
 
-	using namespace Eigen;
+  using namespace Eigen;
+  using boost::math::constants::pi;
 
-	//ボールデータ
-	const auto ball_x = world_.ball().x();
-	const auto ball_y = world_.ball().y();
+  model::command::position_t first_pos;  // ボールからrの円周上
+  model::command::position_t second_pos; // 最終位置(回り込み)
+  model::command::position_t my_pos;     // 今の位置
 
-	const auto ball_vx = world_.ball().vx();
-	const auto ball_vy = world_.ball().vy();   
+  model::command::velocity_t next_vel; // 次の速度
 
-	my_pos = {robot.x(), robot.y(), robot.theta()};
+  const double ball_to_r = 300.; // ボールに対する半径
 
-	//自分->ボール
-	Vector2d v1(ball_x - my_pos.x, ball_y - my_pos.y);
-	v1.normalized();
+  const double acc       = 1000.;                             // 加速度
+  const int acc_count    = 60;                                // 加速時間
+  const double acc_dist  = acc * pow(acc_count / 60., 2) / 2; // 加速にかかる距離
+  const double max_vel   = acc * acc_count / 60.;             // 最高速度
+  const double rot_omega = 90 * pi<double>() / 180.; // 円周上を回るときの角速度
+  const double rot_vel   = ball_to_r * rot_omega;    // 円周上を回るときの速度
+  const int dribble_pow  = 5;                        // ドリブルの強さ
+  double move_omega      = 0.;                       // move_to_ball_回転速度 (定義のみ)
 
-	first_pos = {ball_x - r * v1(0), ball_y - r * v1(1), std::atan2(v1(0),v1(1))};
+  // ボールデータ
+  const double ball_x = world_.ball().x();
+  const double ball_y = world_.ball().y();
 
-	//ボール->ターゲット
-	Vector2d v2(target_x - ball_x, target_y - ball_y); 
-	v2.normalized();
-	
-	second_pos = {ball_x - r * v2(0), ball_y - r * v2(1), std::atan2(v2(0),v2(1))};
+  const double ball_vx = world_.ball().vx();
+  const double ball_vy = world_.ball().vy();
 
-	//それぞれの距離
-	auto dist1 = std::hypot(my_pos.x - first_pos.x, my_pos.y - first_pos.y);
-	auto dist2 = std::hypot(my_pos.x - second_pos.x, my_pos.y - second_pos.y);
+  // 自分の位置
+  my_pos = {robot.x(), robot.y(), robot.theta()};
 
-	//円周上を回る角度
-	auto move_angle = util::wrap_to_pi(v2(2) - v1(2));
+  // 自分->ボール
+  Vector2d v1(ball_x - my_pos.x, ball_y - my_pos.y);
+  v1              = v1.normalized();
+  double v1_theta = std::atan2(v1(1), v1(0));
 
-	//初期設定
-	if (phase == 0) {
-		target_x = 4500.;
-		target_y = 0.;
+  // ボール->ターゲット
+  Vector2d v2(target_x_ - ball_x, target_y_ - ball_y);
+  v2              = v2.normalized();
+  double v2_theta = std::atan2(v2(1), v2(0));
 
-		//first_posまでの 距離が長いとき->select = 1, 距離が短いとき->select = 2
-		if (dist1 >= acc_dist * 2) {
-			p1_count = acc_count * 2 + (dist1 - acc_dist * 2) / max_vel;
-			select = 1;
-		}
-		else {
-			p1_count = std::sqrt(2 * (dist1 / 2.) / acc) * 60 * 2;
-			select = 2;
-		}
+  if (!init_flag_) {
+    count_     = 0;
+    sub_count_ = 0;
+    init_flag_ = true;
 
-		p2_count = std::abs(move_angle) / (rot_omega / 60.);
-		phase = 1;
-	}
+    if ((std::abs(util::wrap_to_pi(pi<double>() + v1_theta - v2_theta))) <
+        (90 * pi<double>() / 180.)) {
+      wrap_flag_ = true;
+      if (util::wrap_to_pi(pi<double>() + v1_theta - v2_theta) > 0) {
+        sign_flag_ = true;
+      }
+    }
+  }
 
-	//first_posに移動
-	if (phase == 1) {
-		count++;
-		auto omega1 = ( util::wrap_to_pi(first_pos.theta - my_pos.theta) ) / (p1_count - count);
+  // 位置の決定
+  if (!wrap_flag_) {
+    first_pos = {ball_x - ball_to_r * std::cos(v1(0)), ball_y - ball_to_r * sin(v1(1)),
+                 v1_theta};
+  } else {
+    if (sign_flag_) {
+      first_pos = {ball_x - ball_to_r * std::sin(v2_theta), ball_y + ball_to_r * cos(v2_theta),
+                   v2_theta - 90 * pi<double>() / 180.};
+    } else {
+      first_pos = {ball_x + ball_to_r * std::sin(v2_theta), ball_y - ball_to_r * cos(v2_theta),
+                   v2_theta + 90 * pi<double>() / 180.};
+    }
+  }
 
-			//距離が長いとき
-			if (select == 1) {
-				if (count <= acc_count) {
-					next_vel = {acc * count / 60. * std::cos(v1(2)), acc * count / 60. * std::sin(v1(2)), omega1};
-				}
+  double move_theta =
+      std::atan2(first_pos.y - my_pos.y, first_pos.x - my_pos.x); // move_to_ball_角度
 
-				else if (count > acc_count && dist1 >= acc_dist) {
-									next_vel = {max_vel * std::cos(v1(2)), max_vel * std::sin(v1(2)), omega1};
-				}
+  second_pos = {ball_x - ball_to_r * std::cos(v2(0)), ball_y - ball_to_r * std::sin(v2(1)),
+                v2_theta};
 
-				else if (dist1 < acc_dist) {
-					sub_count++;
-					if (sub_count <= acc_count) {
-							next_vel = {acc * (acc_count - sub_count) / 60. * std::cos(v1(2)), acc * (acc_count - sub_count) / 60. * std::sin(v1(2)), omega1};
-					}
-					if (dist1 < 100) {
-						phase = 2;
-					}
-					else {
-						next_vel = {(first_pos.x - my_pos.x) / 10., (first_pos.y - my_pos.y) / 10., (util::wrap_to_pi(first_pos.theta - my_pos.theta)) / 10.};
-					}
-				}
-			}
+  // それぞれの距離
+  double dist1 = std::hypot(my_pos.x - first_pos.x, my_pos.y - first_pos.y);
+  double dist2 = std::hypot(my_pos.x - second_pos.x, my_pos.y - second_pos.y);
 
-			//距離が短いとき
-			else if (select == 2) {
-				if (count <= p1_count / 2) {
-					next_vel = {acc * count / 60. * std::cos(v1(2)), acc * count / 60. * std::sin(v1(2)), omega1};
-				}
-				else {
-					sub_count++;
-					if (sub_count < p1_count) {
-						next_vel = {acc * (p1_count / 2. - sub_count) / 60. * std::cos(v1(2)), acc * (p1_count / 2. - sub_count) / 60. * std::sin(v1(2)), omega1};
-					}
-					if(dist1 < 100) {
-						count = 0;
-						sub_count = 0;
-						phase = 2;
-					}
-				}
-			}
-	}
+  // 回転角度
+  double move_angle = util::wrap_to_pi(first_pos.theta - my_pos.theta);
+  double wrap_angle = util::wrap_to_pi(second_pos.theta - my_pos.theta);
 
-	else if (phase == 2){
-		count++;
-		if (count <= p2_count) {
-			if (move_angle >= 0) {
-				next_vel = {rot_vel * std::sin(v1(2)), -rot_vel * std::cos(v1(2)), rot_omega / 60.};
-			}
-			else {
-				next_vel = {-rot_vel * std::sin(v1(2)), rot_vel * std::cos(v1(2)), -rot_omega / 60.};
-			}
-		}
+  switch (mode_) {
+    // first_posに移動
+    case mode::move_to_ball:
+      count_++;
+      move_omega = move_angle / 1.;
 
-		else if (dist2 > 30 || std::abs(move_angle) > 1) {
-			next_vel = {0,0,0};
-		}
-		else {
-			next_vel = {0,0,0};
-			phase = 3;
-		}
-	}
+      if (count_ <= acc_count && dist1 > first_dist_ / 2) {
+        next_vel = {acc * count_ / 60. * std::cos(move_theta),
+                    acc * count_ / 60. * std::sin(move_theta), move_omega};
+      }
 
-	//ドリブルしながら直進
-	else if (phase == 3) {
-		next_vel = {300 * std::cos(v1(2)), 300 * std::sin(v1(2)), 0};
-		command.set_dribble(5);
-		fin_flag = true;
-	}
+      else if (count_ > acc_count && dist1 >= acc_dist) {
+        next_vel = {max_vel * std::cos(move_theta), max_vel * std::sin(move_theta), move_omega};
+      }
 
-	//ボールの速度を加算
-	next_vel = {next_vel.vx + ball_vx, next_vel.vy + ball_vy, next_vel.omega};
-	command.set_velocity(next_vel);
+      else if (dist1 < acc_dist && sub_count_ <= acc_count) {
+        sub_count_++;
+        next_vel = {acc * (acc_count - sub_count_) / 60. * std::cos(move_theta),
+                    acc * (acc_count - sub_count_) / 60. * std::sin(move_theta), move_omega};
+      }
 
-	return command;
+      // 位置調整
+      else {
+        next_vel = {(first_pos.x - my_pos.x) / 5., (first_pos.y - my_pos.y) / 5.,
+                    move_angle / 5.};
+
+        if (dist1 < 100) {
+          mode_      = mode::wraparound;
+          count_     = 0;
+          sub_count_ = 0;
+        }
+      }
+      break;
+
+    // 回り込み
+    case mode::wraparound:
+      count_++;
+      if (count_ <= acc_count) {
+        if (wrap_angle >= 0) {
+          next_vel = {rot_vel * std::sin(move_theta), -rot_vel * std::cos(move_theta),
+                      rot_omega};
+        } else {
+          next_vel = {-rot_vel * std::sin(move_theta), rot_vel * std::cos(move_theta),
+                      -rot_omega};
+        }
+      }
+
+      //位置調整
+      else if (dist2 > 50 || std::abs(wrap_angle) > 2 * pi<double>() / 180.) {
+        next_vel = {(second_pos.x - my_pos.x) / 5., (second_pos.y - my_pos.y) / 5.,
+                    wrap_angle / 5.};
+      } else {
+        next_vel = {0, 0, 0};
+        mode_    = mode::dribble;
+      }
+      break;
+
+    // ドリブルしながら直進
+    case mode::dribble:
+      next_vel = {300 * std::cos(v2_theta), 300 * std::sin(v2_theta), 0};
+      command.set_dribble(dribble_pow);
+      fin_flag_ = true;
+      break;
+  }
+
+  // ボールの速度を加算
+  next_vel = {next_vel.vx + ball_vx, next_vel.vy + ball_vy, next_vel.omega};
+  command.set_velocity(next_vel);
+  return command;
 }
 
 bool chase_ball::finished() const {
-	return fin_flag;
+  return fin_flag_;
 }
 
 } // namespace ai_server
--- src/ai_server/game/action/chase_ball.h
+++ src/ai_server/game/action/chase_ball.h
@@ -1,56 +1,38 @@
 #ifndef AI_SERVER_GAME_ACTION_CHASE_BALL_H
 #define AI_SERVER_GAME_ACTION_CHASE_BALL_H
-#include <cmath>
+
 #include "ai_server/model/command.h"
 #include "base.h"
-#include "ai_server/util/math.h"
 
-namespace ai_server{
-namespace game{
-namespace action{
-
-using boost::math::constants::pi;
+namespace ai_server {
+namespace game {
+namespace action {
 
 class chase_ball : public base {
 public:
-	using base::base;
-	model::command execute() override;
-	bool finished() const override;
-	
+  using base::base;
+  enum class mode { move_to_ball, wraparound, dribble } mode_ = mode::move_to_ball;
+  void set_target(double x, double y);
+  model::command execute() override;
+  bool finished() const override;
+
 private:
-	model::command::position_t first_pos;			//ボールからrの円周上
-	model::command::position_t second_pos;		//最終位置
-	model::command::position_t my_pos;				//今の位置
+  double target_x_;
+  double target_y_;
 
-	model::command::velocity_t next_vel;			//次の速度
+  double first_dist_; // 開始時のfirst_posまでの距離
 
-	int phase = 0;
-	int select = 0;
-	int count = 0;
-	int sub_count = 0;
+  int count_     = 0; // 移動時間のカウント
+  int sub_count_ = 0; // 減速時間のカウント
 
-	double target_x = 0;		//ターゲット
-	double target_y = 0;
-
-	int p1_count = 0;		//phase1にかかる時間
-	int p2_count = 0;		//phase2にかかる時間
-
-	const double r = 300;			//ボールに対する半径
-	
-	const double acc = 3000;		//加速度
-	const int acc_count = 60;	//加速時間
-	const double acc_dist = acc * pow(acc_count / 60., 2) / 2;	//加速にかかる距離
-	const double max_vel = acc * acc_count / 60.;	//最高速度
-
-	const double rot_omega = 180 * pi<double>() / 180.;		//円周上を回るときの角速度
-	const double rot_vel = r * rot_omega;		//円周上を回るときの速度
-
-	
-	bool fin_flag = false;
+  bool init_flag_ = false;
+  bool wrap_flag_ = false;
+  bool sign_flag_ = false;
+  bool fin_flag_  = false;
 };
 
 } // namespace ai_server
 } // namespace game
 } // namespace action
 
-#endif
+#endif // AI_SERVER_GAME_ACTION_CHASE_BALL_H
