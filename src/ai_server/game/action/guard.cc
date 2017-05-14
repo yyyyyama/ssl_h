6d8a81e6959ee3193c6e52c8b95e6e0bd72c4303
NakayamaSatoru <adlersschanze@gmail.com>
2017-05-14 22:50:10 +0900

    [change]速度は偏差に比例させてマージンを取るようにした
    マージン大事

--- src/ai_server/game/action/guard.cc
+++ src/ai_server/game/action/guard.cc
@@ -5,36 +5,35 @@
 namespace ai_server {
 namespace game {
 namespace action {
-void guard::move_to(double x,double y,double theta) {
-	pos_.x() = x;
-	pos_.y() = y;
-	theta_ = theta;
+void guard::move_to(double x, double y, double theta) {
+  pos_.x() = x;
+  pos_.y() = y;
+  theta_   = theta;
 }
-model::command::kick_flag_t guard::kick_type(){
-	return kick_type_;
+model::command::kick_flag_t guard::kick_type() {
+  return kick_type_;
 }
 void guard::set_kick_type(const model::command::kick_flag_t& kick_type) {
   kick_type_ = kick_type;
 }
-void guard::set_dribble(int dribble){
-	dribble_ = dribble;
+void guard::set_dribble(int dribble) {
+  dribble_ = dribble;
 }
-int guard::dribble(){
-	return dribble_;
+int guard::dribble() {
+  return dribble_;
 }
 model::command guard::execute() {
-
-	//速度
-	const auto velocity = 3000.0;	
+  //速度
+  const auto velocity = 3000.0;
   //それぞれ自機を生成
   model::command command(id_);
 
-	//キックフラグを立てて置く
-	command.set_kick_flag(kick_type_);
+  //キックフラグを立てて置く
+  command.set_kick_flag(kick_type_);
 
-	command.set_dribble(dribble_);
+  command.set_dribble(dribble_);
 
-	flag_ = false;
+  flag_ = false;
 
   const auto robots = is_yellow_ ? world_.robots_yellow() : world_.robots_blue();
   if (!robots.count(id_)) {
@@ -44,29 +43,33 @@ model::command guard::execute() {
 
   const auto robot = robots.at(id_);
   const Eigen::Vector2d robot_pos{robot.x(), robot.y()};
-	const auto robot_theta = robot.theta();
-	const auto omega = theta_ - robot_theta;
-//	const Eigen::Vector2d vec{((pos_ - robot_pos).normalized())*velocity};
-	auto c = 10.0;
-	if(((pos_ - robot_pos)*c).norm()>2000){
-		c = 5.0;
+  const auto robot_theta = robot.theta();
+  const auto omega       = theta_ - robot_theta;
+  //	const Eigen::Vector2d vec{((pos_ - robot_pos).normalized())*velocity};
+  auto c = 10.0;
+  if (((pos_ - robot_pos) * c).norm() > 1400.0) {
+    c = 5.0;
+  }
+  const Eigen::Vector2d vec{(pos_ - robot_pos) * c};
+
+  //位置のマージン
+  //const auto margin = 0.1 * vec.norm();
+  auto margin = 80.0;
+	if(vec.norm()<1400){
+		margin = 10.0;
 	}
-	const Eigen::Vector2d vec{(pos_ - robot_pos)*c};
-	
-	//const auto margin = 0.1 * vec.norm();
-	const auto margin =50.0;
-	//目標位置に居るなら終わり
+  //目標位置に居るなら終わり
   if (std::pow(robot_pos.x() - pos_.x(), 2) + std::pow(robot_pos.y() - pos_.y(), 2) -
           std::pow(margin, 2) <
       0) {
     command.set_velocity({0.0, 0.0, omega});
-		flag_ = true;
+    flag_ = true;
     return command;
   }
 
-	command.set_velocity({vec.x(),vec.y(),omega});
+  command.set_velocity({vec.x(), vec.y(), omega});
 
-	return command;
+  return command;
 }
 bool guard::finished() const {
   return flag_;
