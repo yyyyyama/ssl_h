1407e55de18d5dbe47f5b0d1a5f283a13722ad74
Sakyo-Hashimoto <sakyo.hashimoto@gmail.com>
2017-06-09 20:27:09 +0900

    コードスタイルの改善

--- src/ai_server/game/agent/regular.cc
+++ src/ai_server/game/agent/regular.cc
@@ -1,7 +1,7 @@
-#include <unordered_map>
+#include <algorithm>
 #include <cmath>
 #include <tuple>
-#include <algorithm>
+#include <unordered_map>
 #include "regular.h"
 #include "ai_server/model/robot.h"
 #include "ai_server/model/command.h"
@@ -11,63 +11,56 @@ namespace game {
 namespace agent {
 
 regular::regular(const model::world& world, bool is_yellow,
-                 const std::vector<unsigned int>& ids)
-    : base(world, is_yellow), ids_(ids) {
-  ball_chase_     = true;
-  chase_finished_ = false;
-  kick_finished   = false;
-
+                 const std::vector<unsigned int> ids)
+    : base(world, is_yellow), ids_(ids),is_chase_(true),chase_finished_(false),kick_finished_(false) {
   const auto ball = world_.ball();
-  chase_ball_id_  = nearest_robot_id(ball.x(), ball.y(), ids_); //ボール追従ロボ登録
-  set_marking_(chase_ball_id_, ball_chase_);
+  chase_id_  = nearest_robot_id(ball.x(), ball.y(), ids_); //ボール追従ロボ登録
+  set_marking(chase_id_, is_chase_);
 }
 
 bool regular::ball_chase() const {
-  return ball_chase_;
+  return is_chase_;
 }
 
 void regular::set_ball_chase(bool ball_chase) {
-  if (ball_chase_ != ball_chase) {
-    ball_chase_ = ball_chase;
+  if (is_chase_ != ball_chase) {
+    is_chase_ = ball_chase;
 
-    if (ball_chase_) {
+    if (is_chase_) {
       chase_finished_ = false;
-      kick_finished   = false;
+      kick_finished_   = false;
       const auto ball = world_.ball();
-      chase_ball_id_ = nearest_robot_id(ball.x(), ball.y(), ids_); //ボール追従ロボ登録
+      chase_id_ = nearest_robot_id(ball.x(), ball.y(), ids_); // ボール追従ロボ登録
     }
 
-    set_marking_(chase_ball_id_, ball_chase_);
+    set_marking(chase_id_, is_chase_);
   }
 }
 
 std::vector<std::shared_ptr<action::base>> regular::execute() {
   std::vector<std::shared_ptr<action::base>> actions;
 
-  //ボールを追いかける
-  if (ball_chase_) {
+  // ボールを追いかける
+  if (is_chase_) {
     if (chase_finished_) {
-      if (kick_finished) {
+      if (kick_finished_) {
         // ReTry
         const auto ball                 = world_.ball();
-        unsigned int tmp_chase_ball_id_ = nearest_robot_id(ball.x(), ball.y(), ids_);
+        unsigned int tmp_chase_id = nearest_robot_id(ball.x(), ball.y(), ids_);
 
-        if (chase_ball_id_ != tmp_chase_ball_id_) {
-          std::printf("ReTry!!!!!!!!!!!!!!\n\n");
-          chase_ball_id_  = tmp_chase_ball_id_;
+        if (chase_id_ != tmp_chase_id) {
+          chase_id_  = tmp_chase_id;
           chase_finished_ = false;
-          kick_finished   = false;
-          set_marking_(chase_ball_id_, ball_chase_);
+          kick_finished_   = false;
+          set_marking(chase_id_, is_chase_);
         }
 
       } else {
-        std::printf("Kick!\n");
         // Kick
         kick_action_->kick_to(world_.field().x_max(), 0.0);
-        kick_action_->set_kick_type(std::tuple<model::command::kick_type_t, double>(
-            model::command::kick_type_t::line, 50.0));
+        kick_action_->set_kick_type({model::command::kick_type_t::line, 50.0});
         kick_action_->set_mode(action::kick_action::mode::goal);
-        kick_finished = kick_action_->finished();
+        kick_finished_ = kick_action_->finished();
         actions.push_back(kick_action_);
       }
     } else {
@@ -83,34 +76,33 @@ std::vector<std::shared_ptr<action::base>> regular::execute() {
   return actions;
 }
 
-void regular::set_marking_(unsigned int& chase_ball_id, bool ball_chase) {
+void regular::set_marking(unsigned int& chase_ball_id, bool ball_chase) {
   const auto those_robots = !is_yellow_ ? world_.robots_yellow() : world_.robots_blue();
-  std::vector<unsigned int> this_unadded_ids; //まだ動作の割り当てられていない味方ロボットのID
-  std::vector<unsigned int> those_ids; //敵ロボットのID
-  unsigned int added_id_ = ids_.at(0); //ある動作を割り当てられる候補のロボットのID
+  std::vector<unsigned int> unadded_ids;  // まだ動作の割り当てられていない味方ロボットのID
+  std::vector<unsigned int> those_ids; // 敵ロボットのID
+  unsigned int tmp_id = ids_.at(0); // ある動作を割り当てられる候補のロボットのID
 
   mark_actions_.erase(mark_actions_.begin(), mark_actions_.end()); //リセット
 
-  //敵ロボットのIDを登録
+  // 敵ロボットのIDを登録
   for (auto that_rob : those_robots) {
     those_ids.push_back(that_rob.first);
   }
 
-  //マーキング担当のロボットを登録
+  // マーキング担当のロボットを登録
   if (ball_chase) {
-    std::printf("\x1b[44mChaseBall ID : %d\x1b[49m\n\n", chase_ball_id);
     chase_ball_  = std::make_shared<action::chase_ball>(world_, is_yellow_, chase_ball_id);
     kick_action_ = std::make_shared<action::kick_action>(world_, is_yellow_, chase_ball_id);
   }
 
   for (auto id : ids_) {
-    if (!(ball_chase && id == chase_ball_id_)) {
-      this_unadded_ids.push_back(id);
+    if (!(ball_chase && id == chase_id_)) {
+      unadded_ids.push_back(id);
     }
   }
 
-  //対象敵のIDと重要度を登録し、重要度の高い順にソート
-  std::vector<regular::that_robot_importance_> that_importance_list;
+  // 対象敵のIDと重要度を登録し、重要度の高い順にソート
+  std::vector<regular::id_importance_> that_importance_list;
   for (auto id : those_ids) {
     that_importance_list.push_back(
         {id, -1.0 * std::hypot(those_robots.at(id).x() - world_.field().x_min(),
@@ -118,18 +110,16 @@ void regular::set_marking_(unsigned int& chase_ball_id, bool ball_chase) {
   }
   std::sort(that_importance_list.begin(), that_importance_list.end());
 
-  //マーキングを担当するロボットを割り当て
+  // マーキングを担当するロボットを割り当て
   for (auto that_robot : that_importance_list) {
-    if (!this_unadded_ids.empty()) {
-      added_id_ = nearest_robot_id(those_robots.at(that_robot.id).x(),
-                                   those_robots.at(that_robot.id).x(), this_unadded_ids);
-      marking_ = std::make_shared<action::marking>(world_, is_yellow_, added_id_);
+    if (!unadded_ids.empty()) {
+      tmp_id = nearest_robot_id(those_robots.at(that_robot.id).x(),
+                                   those_robots.at(that_robot.id).x(), unadded_ids);
+      marking_ = std::make_shared<action::marking>(world_, is_yellow_, tmp_id);
       marking_->mark_robot(that_robot.id);
       marking_->set_mode(action::marking::mark_mode::shoot_block);
       marking_->set_radius(300.0);
       mark_actions_.push_back(marking_);
-      std::printf("\x1b[44m'%d'\x1b[49m marks to \x1b[44m'%d'\x1b[49m\n\n", added_id_,
-                  that_robot.id); // debug
     } else {
       break;
     }
--- src/ai_server/game/agent/regular.h
+++ src/ai_server/game/agent/regular.h
@@ -2,9 +2,9 @@
 #define AI_SERVER_GAME_AGENT_REGULAR_H
 
 #include "base.h"
-#include "ai_server/game/action/marking.h"
 #include "ai_server/game/action/chase_ball.h"
 #include "ai_server/game/action/kick_action.h"
+#include "ai_server/game/action/marking.h"
 
 namespace ai_server {
 namespace game {
@@ -12,35 +12,35 @@ namespace agent {
 
 class regular : public base {
 public:
-  regular(const model::world& world, bool is_yellow, const std::vector<unsigned int>& ids);
+  regular(const model::world& world, bool is_yellow, const std::vector<unsigned int> ids);
   bool ball_chase() const;
-  void set_ball_chase(bool ball_chase); //全てのロボットをマーキングにする時はfalse
+  void set_ball_chase(bool ball_chase); // 全てのロボットをマーキングにする時はfalse
   std::vector<std::shared_ptr<action::base>> execute() override;
 
 private:
-  const std::vector<unsigned int>& ids_;
-  bool ball_chase_;
-  unsigned int chase_ball_id_;
+  const std::vector<unsigned int> ids_;
+  bool is_chase_;
+  unsigned int chase_id_;
   bool chase_finished_;
-  bool kick_finished;
+  bool kick_finished_;
   std::vector<std::shared_ptr<action::marking>> mark_actions_;
 
-  //マーキング相手を組み直す
-  void set_marking_(unsigned int& chase_ball_id, bool is_ball_chase);
+  // マーキング相手を組み直す
+  void set_marking(unsigned int& chase_ball_id, bool is_ball_chase);
 
-  //ターゲットに最も近いロボットIDを返す(can_idsがconstになってない方は、can_idsから返り値の要素が削除される）
+  // ターゲットに最も近いロボットIDを返す(can_idsがconstになってない方は、can_idsから返り値の要素が削除される）
   unsigned int nearest_robot_id(double target_x, double target_y,
                                 std::vector<unsigned int>& can_ids) const;
   unsigned int nearest_robot_id(double target_x, double target_y,
                                 const std::vector<unsigned int>& can_ids) const;
 
-  // IDと優先度
-  struct that_robot_importance_ {
+  // IDと重要度
+  struct id_importance_ {
     // ID, 重要度
     unsigned int id;
     double importance;
 
-    bool operator<(const that_robot_importance_& next) const {
+    bool operator<(const id_importance_& next) const {
       return importance > next.importance; //ソート基準=重要度が高い順
     }
   };
