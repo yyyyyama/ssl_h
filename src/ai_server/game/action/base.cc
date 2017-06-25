8e367d7b289176982c405a1a328d142292a3f49a
Poe <poe@localhost.localdomain>
2017-06-25 11:05:55 +0900

    [update]actionのbaseにおいてidを受け取れる用に変更
    id欲しかったよね

--- src/ai_server/game/action/base.cc
+++ src/ai_server/game/action/base.cc
@@ -7,6 +7,9 @@ namespace action {
 base::base(const model::world& world, bool is_yellow, unsigned int id)
     : world_(world), is_yellow_(is_yellow), id_(id) {}
 
+unsigned int base::id() const {
+  return id_;
+}
 } // namespace action
 } // namespace game
 } // namespace ai_server
