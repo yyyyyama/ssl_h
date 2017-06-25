ab4556f2873360ce4231b4b6722a45c7d6207430
Kenta Sato <tosainu.maple@gmail.com>
2017-06-25 13:42:50 +0900

    フィールドのデータを更新するクラスの実装

---
+++ src/ai_server/model/updater/field.h
@@ -0,0 +1,39 @@
+#ifndef AI_SERVER_MODEL_WORLD_UPDATER_FIELD_H
+#define AI_SERVER_MODEL_WORLD_UPDATER_FIELD_H
+
+#include <shared_mutex>
+#include "ai_server/model/field.h"
+
+// 前方宣言
+namespace ssl_protos {
+namespace vision {
+class Geometry;
+}
+}
+
+namespace ai_server {
+namespace model {
+namespace updater {
+
+/// @class   field
+/// @brief   SSL-VisionのGeometryパケットでフィールドの情報を更新する
+class field {
+  mutable std::shared_timed_mutex mutex_;
+  model::field field_;
+
+public:
+  field();
+
+  /// @brief          Geometryパケットを処理し, フィールドの情報を更新する
+  /// @param geometry SSL-VisionのGeometryパケット
+  void update(const ssl_protos::vision::Geometry& geometry);
+
+  /// @brief          値を取得する
+  model::field value() const;
+};
+
+} // namespace updater
+} // namespace model
+} // namespace ai_server
+
+#endif // AI_SERVER_MODEL_WORLD_UPDATER_FIELD_H
