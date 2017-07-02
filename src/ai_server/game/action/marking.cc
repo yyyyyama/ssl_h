e213f1f533012608064fae00e55b5447c0922496
Poe <poe@localhost.localdomain>
2017-07-02 13:52:17 +0900

    [update]マーキングの種類の追加
    ダイレクトを守のを追加

--- src/ai_server/game/action/marking.cc
+++ src/ai_server/game/action/marking.cc
@@ -1,5 +1,10 @@
 #include <cmath>
 #include <Eigen/Dense>
+#include <boost/geometry/geometry.hpp>
+#include <boost/geometry/geometries/point_xy.hpp>
+#include <boost/geometry/geometries/polygon.hpp>
+#include <boost/geometry/geometry.hpp>
+#include <boost/assign/list_of.hpp>
 
 #include "ai_server/game/action/marking.h"
 #include "ai_server/util/math.h"
@@ -20,19 +25,25 @@ void marking::set_radius(double radius) {
   radius_ = radius;
 }
 model::command marking::execute() {
+  namespace bg  = boost::geometry;
+  using point   = bg::model::d2::point_xy<double>;
+  using polygon = bg::model::polygon<point>;
   using boost::math::constants::pi;
 
   //それぞれ自機と敵機を生成
   model::command ally_robot(id_);
   const auto enemy_robots = is_yellow_ ? world_.robots_blue() : world_.robots_yellow();
+  const auto my_robots    = is_yellow_ ? world_.robots_yellow() : world_.robots_blue();
   //指定されたロボットが見えなかったらその位置で停止
-  if (!enemy_robots.count(enemy_id_)) {
+  if (!enemy_robots.count(enemy_id_) || !my_robots.count(id_)) {
     ally_robot.set_velocity({0.0, 0.0, 0.0});
     return ally_robot;
   }
   const auto& enemy_robot = enemy_robots.at(enemy_id_);
+  const auto& my_robot    = my_robots.at(id_);
   //必要なパラメータ
   const Eigen::Vector2d enemy{enemy_robot.x(), enemy_robot.y()};
+  const Eigen::Vector2d my{my_robot.x(), my_robot.y()};
   const Eigen::Vector2d ball{world_.ball().x(), world_.ball().y()};
   const Eigen::Vector2d goal{world_.field().x_min(), 0.0};
   Eigen::Vector2d tmp_pos{0.0, 0.0};
@@ -46,31 +57,56 @@ model::command marking::execute() {
       position = (-ratio * enemy + ball) / tmp;
       tmp_pos  = ball;
       break;
-		case mark_mode::corner_block:
-      ratio      = radius_/((enemy - ball).norm()+radius_); //ボール - 目標位置の比
-      position = (-ratio * enemy + 1*ball) / (1-ratio);
+    case mark_mode::corner_block:
+      ratio    = radius_ / ((enemy - ball).norm() + radius_); //ボール - 目標位置の比
+      position = (-ratio * enemy + 1 * ball) / (1 - ratio);
       tmp_pos  = ball;
-			break;
+      break;
     case mark_mode::shoot_block:                 //シュートを阻止
       const auto length = (goal - enemy).norm(); //敵機とゴールの距離
-      tmp               = std::signbit(1400 - length / 2)
+      tmp               = std::signbit(1500 - length / 2)
                 ? 0
-                : 1400 - length / 2; //敵機-ゴール中央の中間地点とゴールラインの差
+                : 1500 - length / 2; //敵機-ゴール中央の中間地点とゴールラインの差
       ratio    = (length / 2 + tmp) / length;
       position = (1 - ratio) * goal + ratio * enemy;
       tmp_pos  = enemy;
   }
-
+  {
+    //移動経路に他のものがあったらよける
+    //判定の幅
+    const auto mergin1 = 300.0;
+    const auto mergin2 = 600.0;
+    //軌道の三角
+    polygon poly;
+    bg::exterior_ring(poly) = boost::assign::list_of<point>(my.x(), my.y())(
+        std::get<0>(util::move(my, position, mergin1)).x(),
+        std::get<0>(util::move(my, position, mergin1)).y())(
+        std::get<1>(util::move(my, position, mergin1)).x(),
+        std::get<1>(util::move(my, position, mergin1)).y())(my.x(), my.y());
+    const point p_e(enemy.x(), enemy.y());
+    const point p_b(ball.x(), ball.y());
+    if (!bg::disjoint(p_e, poly)) {
+      const auto tmp_e  = util::move(my, enemy, mergin2);
+      const auto tmp_e1 = std::get<0>(tmp_e);
+      const auto tmp_e2 = std::get<1>(tmp_e);
+      position          = std::abs(tmp_e1.y()) < std::abs(tmp_e2.y()) ? tmp_e1 : tmp_e2;
+    } else if (!bg::disjoint(p_b, poly)) {
+      const auto tmp_b  = util::move(my, ball, mergin2);
+      const auto tmp_b1 = std::get<0>(tmp_b);
+      const auto tmp_b2 = std::get<1>(tmp_b);
+      position          = std::abs(tmp_b1.y()) < std::abs(tmp_b2.y()) ? tmp_b1 : tmp_b2;
+    }
+  }
   //向きをボールの方へ
   const auto theta = util::wrap_to_2pi(
       std::atan2(position.y() - tmp_pos.y(), position.x() - tmp_pos.x()) + pi<double>());
 
-	//目標位置が外側に行ったらその場で停止
-	if(std::abs(position.x())>4500.0 || std::abs(position.y())>3000.0){
+  //目標位置が外側に行ったらその場で停止
+  if (std::abs(position.x()) > 4500.0 || std::abs(position.y()) > 3000.0) {
     ally_robot.set_velocity({0.0, 0.0, 0.0});
     return ally_robot;
-	}
-	
+  }
+
   //計算した値を自機にセット
   ally_robot.set_position({position.x(), position.y(), theta});
 
