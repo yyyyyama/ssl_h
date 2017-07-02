c0f3c15174c15771f09bab6d8bf9bf59873f58b3
Poe <poe@localhost.localdomain>
2017-07-02 12:49:08 +0900

    [update]三角形の頂点を求める関数の追加
    2点与えられたとき,残りの1点を求めるのを関数化した

--- src/ai_server/util/math.h
+++ src/ai_server/util/math.h
@@ -3,6 +3,8 @@
 
 #include <boost/math/constants/constants.hpp>
 #include <cmath>
+#include <Eigen/Dense>
+#include <tuple>
 
 namespace ai_server {
 namespace util {
@@ -32,6 +34,35 @@ T wrap_to_pi(T r) {
     wrapped += two_pi<T>();
   }
   return wrapped;
+}
+
+template <class T>
+auto move(Eigen::Vector2d begin, Eigen::Vector2d end, T shift) {
+  //移動した量
+  const Eigen::Vector2d move{begin};
+
+  //計算の為に中心にずらした場合の座標
+  Eigen::Vector2d after_begin{begin - move};
+
+  Eigen::Vector2d after_end{end - move};
+
+  // x軸から角度
+  const auto alpha = util::wrap_to_2pi(
+      std::atan2(after_end.y() - after_begin.y(), after_end.x() - after_begin.x()));
+  //回転行列
+  const Eigen::Rotation2Dd rotate(alpha);
+
+  after_end.x() = (after_end - after_begin).norm();
+  after_end.y() = 0.0;
+
+  //移動した先での仮の座標
+  const Eigen::Vector2d tmp1(after_end.x(), shift);
+  const Eigen::Vector2d tmp2(tmp1.x(), tmp1.y() * (-1));
+
+  std::tuple<Eigen::Vector2d, Eigen::Vector2d> t =
+      std::make_tuple((rotate * tmp1) + move, (rotate * tmp2) + move);
+  //回転した後の正しい座標
+  return t;
 }
 }
 }
