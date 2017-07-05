84b7de19a6dfe7165dceac3e39e3405fadb11d94
Poe <poe@localhost.localdomain>
2017-07-05 18:44:38 +0900

    [clean]コードの成形

--- src/ai_server/util/math/geometry.h
+++ src/ai_server/util/math/geometry.h
@@ -12,15 +12,17 @@ namespace util {
 /// @param apex ある直線についての始点
 /// @param middle_base ある直線についての終点
 /// @param shift 終点からずらしたい長さ
-/// @return std::tuple<Eigen::Vector2d,Eigen::Vector2d>{終点から右にずらした点,終点から左にずらした点}
+/// @return
+/// std::tuple<Eigen::Vector2d,Eigen::Vector2d>{終点から右にずらした点,終点から左にずらした点}
 template <class T, std::enable_if_t<std::is_floating_point<T>::value, std::nullptr_t> = nullptr>
-auto calc_isosceles_vertexes(const Eigen::Matrix<T, 2, 1>& apex, const Eigen::Matrix<T, 2, 1>& middle_base,T shift) {
+auto calc_isosceles_vertexes(const Eigen::Matrix<T, 2, 1>& apex,
+                             const Eigen::Matrix<T, 2, 1>& middle_base, T shift) {
   using boost::math::constants::half_pi;
 
   const Eigen::Rotation2Dd rotate(half_pi<double>());
   //正規化した物をpi/2回転させる
   const auto normalize = rotate * ((apex - middle_base).normalized());
-	
+
   //+-shiftずらして終点に足す
   return std::make_tuple((middle_base + normalize * shift), (middle_base + normalize * -shift));
 }
