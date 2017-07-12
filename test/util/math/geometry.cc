b479d9e607a2c466fa39615645fc6441e14b989a
Poe <poe@localhost.localdomain>
2017-07-12 17:43:49 +0900

    [change]よくわからない

--- test/util/math/geometry.cc
+++ test/util/math/geometry.cc
@@ -12,13 +12,13 @@ BOOST_AUTO_TEST_SUITE(geometry)
 BOOST_AUTO_TEST_CASE(test001) {
   const auto tmp1 = util::math::calc_isosceles_vertexes(Eigen::Vector2d{0.0, 0.0},
                                                         Eigen::Vector2d{0.0, 1.0}, 1.0);
-  BOOST_TEST(std::get<0>(tmp1) == Eigen::Vector2d(-1.0, 1.0));
-  BOOST_TEST(std::get<1>(tmp1) == Eigen::Vector2d(1.0, 1.0));
+  BOOST_TEST(std::get<0>(tmp1) == Eigen::Vector2d(1.0, 1.0));
+  BOOST_TEST(std::get<1>(tmp1) == Eigen::Vector2d(-1.0, 1.0));
 
   const auto tmp2 = util::math::calc_isosceles_vertexes(Eigen::Vector2d{0.0, 1.0},
                                                         Eigen::Vector2d{0.0, 0.0}, 1.0);
-  BOOST_TEST(std::get<0>(tmp2) == Eigen::Vector2d(-1.0, 0.0));
-  BOOST_TEST(std::get<1>(tmp2) == Eigen::Vector2d(1.0, 0.0));
+  BOOST_TEST(std::get<0>(tmp2) == Eigen::Vector2d(1.0, 0.0));
+  BOOST_TEST(std::get<1>(tmp2) == Eigen::Vector2d(-1.0, 0.0));
 }
 
 BOOST_AUTO_TEST_SUITE_END()
