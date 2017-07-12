be1e1954543a5fc9a2c9789a250b1b355ce7b6ac
Poe <poe@localhost.localdomain>
2017-07-12 17:52:25 +0900

    [fix]原因特定のため取り敢えず

--- src/ai_server/util/math/geometry.h
+++ src/ai_server/util/math/geometry.h
@@ -15,16 +15,16 @@ namespace math {
 /// @param middle_base ある直線についての終点
 /// @param shift 終点からずらしたい長さ
 /// @return
-/// std::tuple<Eigen::Matrix<T,2,1>,Eigen::Matrix<T,2,1>>{終点から右にずらした点,終点から左にずらした点}
+/// std::tuple<Eigen::Vector2d,Eigen::Vector2d>{終点から右にずらした点,終点から左にずらした点}
 template <class T, std::enable_if_t<std::is_floating_point<T>::value, std::nullptr_t> = nullptr>
-std::tuple<Eigen::Matrix<T, 2, 1>, Eigen::Matrix<T, 2, 1>> calc_isosceles_vertexes(
-    const Eigen::Matrix<T, 2, 1>& apex, const Eigen::Matrix<T, 2, 1>& middle_base, T shift) {
-  const Eigen::Matrix<T, 2, 1> move{apex};
+std::tuple<Eigen::Vector2d, Eigen::Vector2d> calc_isosceles_vertexes(
+    const Eigen::Vector2d& apex, const Eigen::Vector2d& middle_base, T shift) {
+  const Eigen::Vector2d move{apex};
 
   //計算の為に中心にずらした場合の座標
-  Eigen::Matrix<T, 2, 1> after_apex{apex - move};
+  Eigen::Vector2d after_apex{apex - move};
 
-  Eigen::Matrix<T, 2, 1> after_middle_base{middle_base - move};
+  Eigen::Vector2d after_middle_base{middle_base - move};
 
   // x軸から角度
   const auto alpha = util::wrap_to_2pi(std::atan2(after_middle_base.y() - after_apex.y(),
@@ -36,8 +36,8 @@ std::tuple<Eigen::Matrix<T, 2, 1>, Eigen::Matrix<T, 2, 1>> calc_isosceles_vertex
   after_middle_base.y() = 0.0;
 
   //移動した先での仮の座標
-  const Eigen::Matrix<T, 2, 1> tmp1(after_middle_base.x(), shift);
-  const Eigen::Matrix<T, 2, 1> tmp2(tmp1.x(), tmp1.y() * (-1));
+  const Eigen::Vector2d tmp1(after_middle_base.x(), shift);
+  const Eigen::Vector2d tmp2(tmp1.x(), tmp1.y() * (-1));
 
   //回転した後の正しい座標
   return std::make_tuple((rotate * tmp1) + move, (rotate * tmp2) + move);
--- test/util/math/geometry.cc
+++ test/util/math/geometry.cc
@@ -12,8 +12,8 @@ BOOST_AUTO_TEST_SUITE(geometry)
 BOOST_AUTO_TEST_CASE(test001) {
   const auto tmp1 = util::math::calc_isosceles_vertexes(Eigen::Vector2d{0.0, 0.0},
                                                         Eigen::Vector2d{0.0, 1.0}, 1.0);
-  BOOST_TEST(std::get<0>(tmp1) == Eigen::Vector2d(1.0, 1.0));
-  BOOST_TEST(std::get<1>(tmp1) == Eigen::Vector2d(-1.0, 1.0));
+  BOOST_TEST(std::get<0>(tmp1) == Eigen::Vector2d(-1.0, 1.0));
+  BOOST_TEST(std::get<1>(tmp1) == Eigen::Vector2d(1.0, 1.0));
 
   const auto tmp2 = util::math::calc_isosceles_vertexes(Eigen::Vector2d{0.0, 1.0},
                                                         Eigen::Vector2d{0.0, 0.0}, 1.0);
