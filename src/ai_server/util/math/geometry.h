35cd55c5195c430ff3c9fbff58df901ace7a79eb
Poe <poe@localhost.localdomain>
2017-07-12 17:02:16 +0900

    [change]レビューに対する変更
    溜まってた

--- src/ai_server/util/math/geometry.h
+++ src/ai_server/util/math/geometry.h
@@ -15,21 +15,20 @@ namespace math {
 /// @param middle_base ある直線についての終点
 /// @param shift 終点からずらしたい長さ
 /// @return
-/// std::tuple<Eigen::Vector2d,Eigen::Vector2d>{終点から右にずらした点,終点から左にずらした点}
+/// std::tuple<Eigen::Matrix<T,2,1>,Eigen::Matrix<T,2,1>>{終点から右にずらした点,終点から左にずらした点}
 template <class T, std::enable_if_t<std::is_floating_point<T>::value, std::nullptr_t> = nullptr>
-auto calc_isosceles_vertexes(const Eigen::Matrix<T, 2, 1>& apex,
-                             const Eigen::Matrix<T, 2, 1>& middle_base, T shift) {
- using boost::math::constants::half_pi;
- const Eigen::Vector2d move{apex};
+std::tuple<Eigen::Matrix<T, 2, 1>, Eigen::Matrix<T, 2, 1>> calc_isosceles_vertexes(
+    const Eigen::Matrix<T, 2, 1>& apex, const Eigen::Matrix<T, 2, 1>& middle_base, T shift) {
+  const Eigen::Matrix<T, 2, 1> move{apex};
 
   //計算の為に中心にずらした場合の座標
-  Eigen::Vector2d after_apex{apex - move};
+  Eigen::Matrix<T, 2, 1> after_apex{apex - move};
 
-  Eigen::Vector2d after_middle_base{middle_base - move};
+  Eigen::Matrix<T, 2, 1> after_middle_base{middle_base - move};
 
   // x軸から角度
-  const auto alpha = util::wrap_to_2pi(
-      std::atan2(after_middle_base.y() - after_apex.y(), after_middle_base.x() - after_apex.x()));
+  const auto alpha = util::wrap_to_2pi(std::atan2(after_middle_base.y() - after_apex.y(),
+                                                  after_middle_base.x() - after_apex.x()));
   //回転行列
   const Eigen::Rotation2D<T> rotate(alpha);
 
@@ -37,9 +36,9 @@ auto calc_isosceles_vertexes(const Eigen::Matrix<T, 2, 1>& apex,
   after_middle_base.y() = 0.0;
 
   //移動した先での仮の座標
-  const Eigen::Vector2d tmp1(after_middle_base.x(), shift);
-  const Eigen::Vector2d tmp2(tmp1.x(), tmp1.y() * (-1));
-	
+  const Eigen::Matrix<T, 2, 1> tmp1(after_middle_base.x(), shift);
+  const Eigen::Matrix<T, 2, 1> tmp2(tmp1.x(), tmp1.y() * (-1));
+
   //回転した後の正しい座標
   return std::make_tuple((rotate * tmp1) + move, (rotate * tmp2) + move);
 }
