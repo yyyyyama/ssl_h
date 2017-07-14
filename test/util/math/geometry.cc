dae9a9433aef9408f957deb32a05aca9f615238e
Poe <poe@localhost.localdomain>
2017-07-14 15:03:39 +0900

    [change]やっとテスト通ったっぽい

--- test/util/math/geometry.cc
+++ test/util/math/geometry.cc
@@ -12,13 +12,17 @@ BOOST_AUTO_TEST_SUITE(geometry)
 BOOST_AUTO_TEST_CASE(test001, *boost::unit_test::tolerance(0.0000001)) {
   const auto tmp1 = util::math::calc_isosceles_vertexes(Eigen::Vector2d{0.0, 0.0},
                                                         Eigen::Vector2d{0.0, 1.0}, 1.0);
-  BOOST_TEST(std::get<0>(tmp1) == Eigen::Vector2d(-1.0, 1.0));
-  BOOST_TEST(std::get<1>(tmp1) == Eigen::Vector2d(1.0, 1.0));
+  BOOST_TEST(std::get<0>(tmp1).x() == -1.0);
+  BOOST_TEST(std::get<0>(tmp1).y() == 1.0);
+  BOOST_TEST(std::get<1>(tmp1).x() == 1.0);
+  BOOST_TEST(std::get<1>(tmp1).y() == 1.0);
 
   const auto tmp2 = util::math::calc_isosceles_vertexes(Eigen::Vector2d{0.0, 1.0},
                                                         Eigen::Vector2d{0.0, 0.0}, 1.0);
-  BOOST_TEST(std::get<0>(tmp2) == Eigen::Vector2d(1.0, 0.0));
-  BOOST_TEST(std::get<1>(tmp2) == Eigen::Vector2d(-1.0, 0.0));
+  BOOST_TEST(std::get<0>(tmp2).x() == 1.0);
+  BOOST_TEST(std::get<0>(tmp2).y() == 0.0);
+  BOOST_TEST(std::get<1>(tmp2).x() == -1.0);
+  BOOST_TEST(std::get<1>(tmp2).y() == 0.0);
 }
 
 BOOST_AUTO_TEST_SUITE_END()
