c1648cfc28cb6177b031d664fe8128d1ec7d7559
Kenta Sato <tosainu.maple@gmail.com>
2017-07-15 10:50:14 +0900

    receiver::refboxからデータの更新処理を削除

--- src/ai_server/receiver/refbox.h
+++ src/ai_server/receiver/refbox.h
@@ -23,12 +23,10 @@ public:
          const std::string& multicast_addr, short port);
   boost::signals2::connection on_receive(const receive_signal_t::slot_type& slot);
   boost::signals2::connection on_error(const error_signal_t::slot_type& slot);
-  const model::refbox& refbox_data() const;
 
 private:
   void parse_packet(const util::net::multicast::receiver::buffer_t& buffer, std::size_t size);
   util::net::multicast::receiver receiver_;
-  model::refbox refbox_data_;
 
   receive_signal_t received_;
   error_signal_t errored_;
