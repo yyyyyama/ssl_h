d8940bd08aa35bf2e56f9905a5f0a619362e10d7
Toshiki Mimura <tm36104.kto.0724@gmail.com>
2017-07-17 09:59:57 +0900

    baseに速度制限用のメンバ追加
    制限速度のデータメンバと
    制限速度設定用のメンバ関数，
    制限速度を考慮したコンストラクタの実装

--- src/ai_server/controller/base.cc
+++ src/ai_server/controller/base.cc
@@ -1,7 +1,12 @@
+#include <limits>
 #include "base.h"
 
 namespace ai_server {
 namespace controller {
+
+base::base() : velocity_limit_(std::numeric_limits<double>::max()) {}
+
+base::base(const double limit) : velocity_limit_(limit) {}
 
 velocity_t base::operator()(const model::robot& robot, const position_t& setpoint) {
   return update(robot, setpoint);
@@ -9,6 +14,14 @@ velocity_t base::operator()(const model::robot& robot, const position_t& setpoin
 
 velocity_t base::operator()(const model::robot& robot, const velocity_t& setpoint) {
   return update(robot, setpoint);
+}
+
+double base::velocity_limit() const {
+  return velocity_limit_;
+}
+
+void base::set_velocity_limit(const double limit) {
+  velocity_limit_ = limit;
 }
 
 } // namespace controller
