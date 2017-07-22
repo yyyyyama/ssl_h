96c4e77d9abe4ad8e7d98fc9fb7bcb2a4c13d606
Kenta Sato <tosainu.maple@gmail.com>
2017-07-22 23:11:11 +0900

    単一のチームカラーのみを扱うように変更

--- src/ai_server/driver.h
+++ src/ai_server/driver.h
@@ -11,6 +11,7 @@
 
 #include "ai_server/controller/base.h"
 #include "ai_server/model/command.h"
+#include "ai_server/model/team_color.h"
 #include "ai_server/model/updater/world.h"
 #include "ai_server/sender/base.h"
 #include "ai_server/util/time.h"
@@ -18,58 +19,63 @@
 namespace ai_server {
 
 class driver {
-  using controller_ptr = std::unique_ptr<controller::base>;
-  using sender_ptr     = std::shared_ptr<sender::base>;
-
-  /// Driverで行う処理で必要となる各ロボットの情報
-  using driver_param_type = std::tuple<model::command, controller_ptr, sender_ptr>;
-
-  mutable std::mutex mutex_;
-
-  /// 制御部の処理を一定の周期で回すためのタイマ
-  boost::asio::basic_waitable_timer<util::clock_type> timer_;
-
-  /// 制御周期
-  util::duration_type cycle_;
-
-  /// updater::worldの参照
-  const model::updater::world& world_;
-
-  /// 登録された青ロボットの情報
-  std::unordered_map<unsigned int, driver_param_type> robots_blue_params_;
-  /// 登録された黄ロボットの情報
-  std::unordered_map<unsigned int, driver_param_type> robots_yellow_params_;
+  /// Controllerのポインタの型
+  using controller_type = std::unique_ptr<controller::base>;
+  /// Senderのポインタの型
+  using sender_type = std::shared_ptr<sender::base>;
+  /// Driverで行う処理で必要となる各ロボットの情報の型
+  using metadata_type = std::tuple<model::command, controller_type, sender_type>;
 
 public:
   /// @param cycle            制御周期
   /// @param world            updater::worldの参照
+  /// @param color            チームカラー
   driver(boost::asio::io_service& io_service, util::duration_type cycle,
-         const model::updater::world& world);
+         const model::updater::world& world, model::team_color color);
+
+  /// @brief                  現在設定されているチームカラーを取得する
+  model::team_color team_color() const;
+
+  /// @brief                  チームカラーを変更する
+  /// @param color            チームカラー
+  void set_team_color(model::team_color color);
 
   /// @brief                  Driverにロボットを登録する
-  /// @param is_yellow        ロボットの色
   /// @param id               ロボットのID
   /// @param controller       Controller
   /// @param sender           Sender
-  void register_robot(bool is_yellow, unsigned int id, controller_ptr controller,
-                      sender_ptr sender);
+  void register_robot(unsigned int id, controller_type controller, sender_type sender);
 
   /// @brief                  Driverに登録されたロボットを解除する
-  /// @param is_yellow        ロボットの色
   /// @param id               ロボットのID
-  void unregister_robot(bool is_yellow, unsigned int id);
+  void unregister_robot(unsigned int id);
 
   /// @brief                  ロボットへの命令を更新する
-  /// @param is_yellow        ロボットの色
   /// @param command          ロボットへの命令
-  void update_command(bool is_yellow, const model::command& command);
+  void update_command(const model::command& command);
 
 private:
   /// @brief                  cycle_毎に呼ばれる制御部のメインループ
   void main_loop(const boost::system::error_code& error);
 
   /// @brief                  ロボットへの命令をControllerを通してから送信する
-  void process(bool is_yellow, const model::world& world, driver_param_type& driver_param);
+  void process(const model::world& world, metadata_type& metadata);
+
+  mutable std::mutex mutex_;
+
+  /// 制御部の処理を一定の周期で回すためのタイマ
+  boost::asio::basic_waitable_timer<util::clock_type> timer_;
+  /// 制御周期
+  util::duration_type cycle_;
+
+  /// updater::worldの参照
+  const model::updater::world& world_;
+
+  /// チームカラー
+  model::team_color team_color_;
+
+  /// 登録されたロボットの情報
+  std::unordered_map<unsigned int, metadata_type> robots_metadata_;
 };
 
 } // namespace ai_server
--- test/driver.cc
+++ test/driver.cc
@@ -6,36 +6,65 @@
 #include <boost/test/unit_test.hpp>
 
 #include "ai_server/driver.h"
+#include "ai_server/model/team_color.h"
 #include "ai_server/model/updater/world.h"
+
+using namespace std::chrono_literals;
+namespace model = ai_server::model;
+
+BOOST_TEST_DONT_PRINT_LOG_VALUE(model::team_color)
 
 BOOST_AUTO_TEST_SUITE(driver)
 
-BOOST_AUTO_TEST_CASE(register_unregister) {
+BOOST_AUTO_TEST_CASE(set_team_color) {
   boost::asio::io_service io_service{};
   ai_server::model::updater::world wu{};
-  ai_server::driver d{io_service, std::chrono::seconds{1}, wu};
+
+  {
+    // コンストラクタでチームカラーが正しく設定されているか
+    ai_server::driver d{io_service, 1s, wu, model::team_color::blue};
+    BOOST_TEST(d.team_color() == model::team_color::blue);
+
+    // チームカラーが変更できるか
+    d.set_team_color(model::team_color::yellow);
+    BOOST_TEST(d.team_color() == model::team_color::yellow);
+  }
+
+  {
+    ai_server::driver d{io_service, 1s, wu, model::team_color::yellow};
+    BOOST_TEST(d.team_color() == model::team_color::yellow);
+
+    d.set_team_color(model::team_color::blue);
+    BOOST_TEST(d.team_color() == model::team_color::blue);
+  }
+}
+
+BOOST_AUTO_TEST_CASE(register_robot) {
+  boost::asio::io_service io_service{};
+  ai_server::model::updater::world wu{};
+  ai_server::driver d{io_service, std::chrono::seconds{1}, wu, model::team_color::yellow};
 
   ai_server::model::command cmd0{0};
   ai_server::model::command cmd1{1};
 
   // ロボットが登録されていない状態でupdate_command()を呼ぶとエラー
-  BOOST_CHECK_THROW(d.update_command(true, cmd0), std::runtime_error);
-  BOOST_CHECK_THROW(d.update_command(false, cmd1), std::runtime_error);
+  BOOST_CHECK_THROW(d.update_command(cmd0), std::runtime_error);
+  BOOST_CHECK_THROW(d.update_command(cmd1), std::runtime_error);
 
-  d.register_robot(true, 0, nullptr, nullptr);
-  BOOST_CHECK_NO_THROW(d.update_command(true, cmd0));
-  BOOST_CHECK_THROW(d.update_command(false, cmd1), std::runtime_error);
+  // ID0のロボットを登録
+  BOOST_CHECK_NO_THROW(d.register_robot(0, nullptr, nullptr));
+  BOOST_CHECK_NO_THROW(d.update_command(cmd0));
 
-  d.register_robot(false, 1, nullptr, nullptr);
-  BOOST_CHECK_NO_THROW(d.update_command(true, cmd0));
-  BOOST_CHECK_NO_THROW(d.update_command(false, cmd1));
+  // ID1のロボットを登録
+  BOOST_CHECK_NO_THROW(d.register_robot(1, nullptr, nullptr));
+  BOOST_CHECK_NO_THROW(d.update_command(cmd1));
 
   // 登録したロボットが正常に削除されるか
-  d.unregister_robot(true, 0);
-  BOOST_CHECK_THROW(d.update_command(true, cmd0), std::runtime_error);
-
-  d.unregister_robot(false, 1);
-  BOOST_CHECK_THROW(d.update_command(false, cmd1), std::runtime_error);
+  d.unregister_robot(0);
+  BOOST_CHECK_THROW(d.update_command(cmd0), std::runtime_error);
+  BOOST_CHECK_NO_THROW(d.update_command(cmd1));
+  d.unregister_robot(1);
+  BOOST_CHECK_THROW(d.update_command(cmd1), std::runtime_error);
 }
 
 BOOST_AUTO_TEST_SUITE_END()
