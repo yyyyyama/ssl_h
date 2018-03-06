306b5813d7ed0b499d178026df8d6fa7779a08a2
YuseiNaito <naito@localhost.localdomain>
2018-03-06 16:36:58 +0900

    Autonomous Ball Placementのactionをつくった

---
+++ src/ai_server/game/action/autonomous_ball_place.cc
@@ -0,0 +1,159 @@
+#include <chrono>
+#include <cmath>
+#include <boost/math/constants/constants.hpp>
+#include "ai_server/util/math/angle.h"
+#include "ai_server/util/time.h"
+#include "autonomous_ball_place.h"
+#include <iostream>
+
+using boost::math::constants::pi;
+
+namespace ai_server {
+namespace game {
+namespace action {
+
+autonomous_ball_place::autonomous_ball_place(const model::world& world, bool is_yellow,
+                                             unsigned int id)
+    : base(world, is_yellow, id), command_(id) {}
+
+autonomous_ball_place::running_state autonomous_ball_place::state() const {
+  switch (state_) {
+    case running_state::move:
+      std::cout << "move" << std::endl;
+      break;
+    case running_state::hold:
+      std::cout << "hold" << std::endl;
+      break;
+    case running_state::place:
+      std::cout << "place" << std::endl;
+      break;
+    case running_state::wait:
+      std::cout << "wait" << std::endl;
+      break;
+    case running_state::leave:
+      std::cout << "leave" << std::endl;
+      break;
+    case running_state::finished:
+      std::cout << "finished" << std::endl;
+      break;
+  }
+  return state_;
+}
+
+void autonomous_ball_place::place_to(double target_x, double target_y) {
+  target_x_ = target_x;
+  target_y_ = target_y;
+}
+
+model::command autonomous_ball_place::execute() {
+  model::command command_{id_};
+  // 許容誤差(ルール上は10[cm]以内?)
+  const double xy_allow = 50.0;
+  const auto our_robots = is_yellow_ ? world_.robots_yellow() : world_.robots_blue();
+  const auto& robot     = our_robots.at(id_);
+  const auto ball       = world_.ball();
+  // 目標
+  double x_;
+  double y_;
+  auto theta_ =
+      util::math::wrap_to_2pi(std::atan2(target_y_ - ball.y(), target_x_ - ball.x())) +
+      pi<double>();
+  // ロボットがボールを噛んでいるときのロボットのx座標とボールのx座標の差
+  const double distx = std::abs(95.0 * std::cos(theta_));
+  // ロボットとボールの距離
+  const double distance =
+      std::pow(std::pow(robot.x() - ball.x(), 2) + std::pow(robot.y() - ball.y(), 2), 1.0 / 2);
+  const std::chrono::seconds wait_time_(1);
+
+  std::cout << "distance: " << distance << std::endl;
+
+  if (std::abs(ball.x() - target_x_) < xy_allow && std::abs(ball.y() - target_y_) < xy_allow &&
+      distance > 590.0) {
+    // 全条件を満たせば停止
+    finished_ = true;
+    state_    = running_state::finished;
+    command_.set_dribble(0);
+    command_.set_velocity({0.0, 0.0, 0.0});
+  } else if (state_ == running_state::leave) {
+    // ボールから離れる
+    finished_ = false;
+    x_        = ball.x() + 600.0 * ((robot.x() > ball.x()) - (robot.x() < ball.x()));
+    y_        = (robot.x() == ball.x())
+             ? ball.y() + 600.0
+             : ((robot.y() - ball.y()) / (robot.x() - ball.x())) * (x_ - ball.x()) +
+                   ball.y(); // 0除算防止
+    theta_ = util::math::wrap_to_2pi(std::atan2(robot.y() - ball.y(), robot.x() - ball.x())) +
+             pi<double>();
+    command_.set_position({x_, y_, theta_});
+  } else if (state_ == running_state::wait) {
+    // 待機
+    finished_ = false;
+    now_      = std::chrono::system_clock::now();
+    if (wait_flag_) {
+      begin_     = std::chrono::system_clock::now();
+      wait_flag_ = false;
+    }
+    if (std::chrono::duration_cast<std::chrono::seconds>(now_ - begin_) >= wait_time_) {
+      state_ = running_state::leave;
+    }
+  } else if (std::abs(ball.x() - target_x_) < xy_allow &&
+             std::abs(ball.y() - target_y_) < xy_allow) {
+    // 配置完了
+    // wait,leaveをここに入れるかどうか(誤差を考えると入れないほうがスムーズに動作しそう)
+    finished_  = false;
+    state_     = running_state::wait;
+    wait_flag_ = true;
+    command_.set_dribble(0);
+    command_.set_velocity({0.0, 0.0, 0.0});
+  } else if (state_ == running_state::place) {
+    // 配置
+    finished_ = false;
+    state_    = running_state::place;
+    if (target_x_ == ball.x()) { // 0除算防止
+      x_ = target_x_;
+      y_ = target_y_ + 95.0 * ((target_y_ > ball.y()) - (target_y_ < ball.y()));
+    } else {
+      x_ = target_x_ + distx * ((target_x_ > ball.x()) - (target_x_ < ball.x()));
+      y_ = ((target_y_ - ball.y()) / (target_x_ - ball.x())) * (x_ - target_x_) + target_y_;
+    }
+    command_.set_dribble(9);
+    command_.set_position({x_, y_, theta_});
+  } else if (state_ == running_state::hold) { // 以下考え中
+    // ボールを持つ
+    finished_ = false;
+    if (std::pow(ball.x() - first_ballx_, 2) + std::pow(ball.y() - first_bally_, 2) >
+        500.0 /*ボールがある程度動いたら持ったとみなす*/) {
+      state_ = running_state::place;
+    }
+    command_.set_dribble(9);
+    command_.set_position({ball.x(), ball.y(), theta_});
+  } else if (distance < 170.0 /*ボールの目の前か?*/) {
+    finished_    = false;
+    state_       = running_state::hold;
+    first_ballx_ = ball.x();
+    first_bally_ = ball.y();
+    command_.set_velocity({0.0, 0.0, 0.0});
+  } else {
+    // 移動
+    state_       = running_state::move;
+    first_ballx_ = ball.x();
+    first_bally_ = ball.y();
+    x_ = ball.x() + (distx + 50.0) * ((robot.x() > ball.x()) - (robot.x() < ball.x()));
+    y_ = (robot.x() == ball.x())
+             ? ball.y()
+             : ((target_y_ - ball.y()) / (target_y_ - ball.x())) * (x_ - ball.x()) +
+                   ball.y(); // 0除算防止
+    theta_ = util::math::wrap_to_2pi(std::atan2(target_y_ - ball.y(), target_x_ - ball.x())) +
+             pi<double>();
+    command_.set_position({x_, y_, theta_});
+  }
+  std::cout << first_ballx_ << ' ' << first_bally_ << std::endl;
+  return command_;
+}
+
+bool autonomous_ball_place::finished() const {
+  return finished_;
+};
+} // namespace action
+} // namespace game
+} // namespace ai_server
---
+++ src/ai_server/game/action/autonomous_ball_place.h
@@ -0,0 +1,43 @@
+#ifndef AI_SERVER_GAME_ACTION_AUTONOMOUS_BALL_PLACE_H
+#define AI_SERVER_GAME_ACTION_AUTONOMOUS_BALL_PLACE_H
+
+#include <chrono>
+#include "base.h"
+
+namespace ai_server {
+namespace game {
+namespace action {
+
+class autonomous_ball_place : public base {
+public:
+  autonomous_ball_place(const model::world& world, bool is_yellow, unsigned int id);
+
+  void place_to(double target_x, double target_y);
+
+  enum class running_state { move, hold, place, wait, leave, finished };
+
+  model::command execute() override;
+
+  bool can_leave();
+
+  running_state state() const;
+
+  bool finished() const override;
+
+private:
+  model::command command_;
+  running_state state_ = running_state::move;
+  bool finished_       = false;
+  bool wait_flag_      = true;
+  std::chrono::system_clock::time_point begin_;
+  std::chrono::system_clock::time_point now_;
+  double target_x_;
+  double target_y_;
+  double first_ballx_;
+  double first_bally_;
+};
+} // namespace action
+} // namespace game
+} // namespace ai_server
+
+#endif
