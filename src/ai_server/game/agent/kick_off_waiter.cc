b46afade5775881b2485bcd2e8063323e69acb6b
hatokani2 <atsuya887@gmail.com>
2018-03-25 02:48:36 +0900

    kickoff守備で、敵キッカーの向きを追うように変更した (#268)
    * kickoff守備で、敵キッカーの向きを追うように変更した
    
    * 変更し直し
    
    * Revert "変更し直し"
    
    This reverts commit 089bf3e731f7a8f5442260f3750820f5b4b16bd6.
    
    * Revert "kickoff守備で、敵キッカーの向きを追うように変更した"
    
    This reverts commit 77f1fdbf065cba3b32f0b0aae5fc4e12201c0ab7.
    
    * 変更し直
    
    * hypotをstd::hypotに修正

--- src/ai_server/game/agent/kick_off_waiter.cc
+++ src/ai_server/game/agent/kick_off_waiter.cc
@@ -13,21 +13,33 @@ kick_off_waiter::kick_off_waiter(const model::world& world, bool is_yellow,
 std::vector<std::shared_ptr<action::base>> kick_off_waiter::execute() {
   std::vector<std::shared_ptr<action::base>> exe;
   const auto robots = is_yellow_ ? world_.robots_yellow() : world_.robots_blue();
-  auto tmp_ids      = ids_;
-  auto ids_size     = ids_.size();
+
+  //視認可能なロボットをids_から抽出する
+  std::vector<unsigned int> visible_robots;
+  std::copy_if(ids_.cbegin(), ids_.cend(), std::back_inserter(visible_robots),
+               [&robots](auto id) { return robots.count(id); });
+  auto tmp_ids  = visible_robots;
+  auto ids_size = visible_robots.size();
 
   //攻撃側
   if (mode_ == kickoff_mode::attack) {
-    int count       = 2;      //何番目のロボットか判別
+    int count_a     = 2; //何番目のロボットか判別
+    int count_b     = 2;
     double interval = 500.0;  //ロボットの間隔
     double x_line   = -200.0; // xは-200固定
 
-    for (auto it = tmp_ids.begin(); it != tmp_ids.end(); it++, count++) {
+    for (auto it = tmp_ids.begin(); it != tmp_ids.end(); it++) {
       auto move = std::make_shared<action::move>(world_, is_yellow_, *it);
-      if (count % 2) {
-        move->move_to(x_line, world_.field().y_min() + interval * (count / 2), 0);
+      auto a    = std::hypot((x_line - robots.at(*it).x()),
+                     (world_.field().y_min() + interval * count_a - robots.at(*it).y()));
+      auto b    = std::hypot((x_line - robots.at(*it).x()),
+                     (world_.field().y_max() - interval * count_b - robots.at(*it).y()));
+      if (a < b) {
+        move->move_to(x_line, world_.field().y_min() + interval * count_a, 0);
+        count_a++;
       } else {
-        move->move_to(x_line, world_.field().y_max() - interval * (count / 2), 0);
+        move->move_to(x_line, world_.field().y_max() - interval * count_b, 0);
+        count_b++;
       }
       exe.push_back(move);
     }
@@ -47,23 +59,34 @@ std::vector<std::shared_ptr<action::base>> kick_off_waiter::execute() {
           return std::hypot(enemies.at(a).x() - ball.x(), enemies.at(a).y() - ball.y()) <
                  std::hypot(enemies.at(b).x() - ball.x(), enemies.at(b).y() - ball.y());
         });
+    //敵キッカーの座標を取得し,マーキング候補から除外
+    double enemy_theta = 0;
+    double enemy_x     = 0;
+    double enemy_y     = 0;
     if (ball_near_it != enemies_id.end()) {
+      enemy_theta = enemies.at(*ball_near_it).theta();
+      enemy_x     = enemies.at(*ball_near_it).x();
+      enemy_y     = enemies.at(*ball_near_it).y();
       enemies_id.erase(ball_near_it);
     }
 
-    //一体はボールの前に固定
-    double x     = -700;
-    double y     = 0;
-    double theta = 0;
+    //常に敵キッカーの目の前にいるような位置取りにする
+    double r = 750; //センターサークルに入らないような半径
+    double x = r * std::cos(enemy_theta) < -20 ? 700 * std::cos(enemy_theta)
+                                               : -700 * std::cos(enemy_theta);
+    double y     = r * std::sin(enemy_theta);
+    double theta = std::atan2(enemy_y - y, enemy_x - x);
     const auto it =
         std::min_element(tmp_ids.cbegin(), tmp_ids.cend(), [&robots, x, y](auto& a, auto& b) {
           return std::hypot(robots.at(a).x() - x, robots.at(a).y() - y) <
                  std::hypot(robots.at(b).x() - x, robots.at(b).y() - y);
         });
-    auto move = std::make_shared<action::move>(world_, is_yellow_, *it);
-    tmp_ids.erase(it);
-    move->move_to(x, y, theta);
-    exe.push_back(move);
+    if (it != tmp_ids.end()) {
+      auto move = std::make_shared<action::move>(world_, is_yellow_, *it);
+      tmp_ids.erase(it);
+      move->move_to(x, y, theta);
+      exe.push_back(move);
+    }
 
     //敵をセンターラインに近い順にソート
     std::sort(enemies_id.begin(), enemies_id.end(),
@@ -89,10 +112,13 @@ std::vector<std::shared_ptr<action::base>> kick_off_waiter::execute() {
             return std::hypot(robots.at(a).x() - x, robots.at(a).y() - y) <
                    std::hypot(robots.at(b).x() - x, robots.at(b).y() - y);
           });
-      move = std::make_shared<action::move>(world_, is_yellow_, *it);
-      move->move_to(x, y, theta);
-      exe.push_back(move);
-      tmp_ids.erase(it);
+      if (it != tmp_ids.end()) {
+        auto move = std::make_shared<action::move>(world_, is_yellow_, *it);
+        move      = std::make_shared<action::move>(world_, is_yellow_, *it);
+        move->move_to(x, y, theta);
+        exe.push_back(move);
+        tmp_ids.erase(it);
+      }
     }
   }
 
@@ -106,6 +132,6 @@ void kick_off_waiter::set_mode(kickoff_mode mode) {
 kick_off_waiter::kickoff_mode kick_off_waiter::mode() {
   return mode_;
 }
-}
-}
-}
+} // namespace agent
+} // namespace game
+} // namespace ai_server
