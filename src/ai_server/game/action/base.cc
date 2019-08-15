6859505ae4986ab609728763aedca94129974c4b
YuseiNaito <yusei.vitz.787@gmail.com>
2019-08-15 15:39:30 +0900

    Issue/382 using rrt (#404)
    * actionでplannerを使うためのメンバを追加
    
    * plannerが設定されているかを取得する関数を追加

--- src/ai_server/game/action/base.cc
+++ src/ai_server/game/action/base.cc
@@ -10,6 +10,14 @@ base::base(const model::world& world, bool is_yellow, unsigned int id)
 unsigned int base::id() const {
   return id_;
 }
+
+void base::set_path_planner(std::unique_ptr<planner::base> planner) {
+  planner_ = std::move(planner);
+}
+
+bool base::has_path_planner() const {
+  return static_cast<bool>(planner_);
+}
 } // namespace action
 } // namespace game
 } // namespace ai_server
--- src/ai_server/game/action/base.h
+++ src/ai_server/game/action/base.h
@@ -3,6 +3,7 @@
 
 #include "ai_server/model/command.h"
 #include "ai_server/model/world.h"
+#include "ai_server/planner/base.h"
 
 namespace ai_server {
 namespace game {
@@ -26,10 +27,18 @@ public:
   /// @brief                  Actionが完了したか
   virtual bool finished() const = 0;
 
+  /// @brief                  plannerを設定する
+  /// @param planner          使用するplanner
+  void set_path_planner(std::unique_ptr<planner::base> planner);
+
+  /// @brief                  plannerが設定されているか
+  bool has_path_planner() const;
+
 protected:
   const model::world& world_;
   bool is_yellow_;
   unsigned int id_;
+  std::unique_ptr<planner::base> planner_;
 };
 
 } // namespace action
