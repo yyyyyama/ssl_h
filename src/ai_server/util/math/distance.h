196c4e124320b07141c33668c393b69a9659beeb
sakyo_h <sakyo.hashimoto@gmail.com>
2019-12-16 21:46:07 +0900

    Util math distance (#436)
    * Add distance.h
    
    * Add util::math::distance test
    
    * 関数の名前をto_vector.hと合わせた
    
    * テスト更新

---
+++ src/ai_server/util/math/distance.h
@@ -0,0 +1,64 @@
+#ifndef AI_SERVER_UTIL_MATH_DISTANCE_H
+#define AI_SERVER_UTIL_MATH_DISTANCE_H
+
+#include <cmath>
+
+namespace ai_server::util::math {
+
+namespace detail {
+
+template <class T>
+inline auto x(const T& v) -> decltype(v.x) {
+  return v.x;
+}
+
+template <class T>
+inline auto x(const T& v) -> decltype(v.x()) {
+  return v.x();
+}
+
+template <class T>
+inline auto y(const T& v) -> decltype(v.y) {
+  return v.y;
+}
+
+template <class T>
+inline auto y(const T& v) -> decltype(v.y()) {
+  return v.y();
+}
+
+template <class T>
+inline auto z(const T& v) -> decltype(v.z) {
+  return v.z;
+}
+
+template <class T>
+inline auto z(const T& v) -> decltype(v.z()) {
+  return v.z();
+}
+} // namespace detail
+
+/// @brief   xy平面における2点の距離を求める
+/// @param a 点1の座標
+/// @param b 点2の座標
+/// @return  floatまたはdouble
+template <class T, class U>
+inline auto distance(const T& a, const U& b)
+    -> decltype(std::hypot(detail::x(a) - detail::x(b), detail::y(a) - detail::y(b))) {
+  return std::hypot(detail::x(a) - detail::x(b), detail::y(a) - detail::y(b));
+}
+
+/// @brief   xyz空間における2点の距離を求める
+/// @param a 点1の座標
+/// @param b 点2の座標
+/// @return  floatまたはdouble
+template <class T, class U>
+inline auto distance3d(const T& a, const U& b)
+    -> decltype(std::hypot(detail::x(a) - detail::x(b), detail::y(a) - detail::y(b),
+                           detail::z(a) - detail::z(b))) {
+  return std::hypot(detail::x(a) - detail::x(b), detail::y(a) - detail::y(b),
+                    detail::z(a) - detail::z(b));
+}
+} // namespace ai_server::util::math
+
+#endif
