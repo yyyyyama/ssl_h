3f4845c4767f933cabead2039e0c59072a946b73
YuseiNaito <yusei.vitz.787@gmail.com>
2020-06-21 23:42:00 +0900

    action::receiveの更新 (#560)
    * set_positionを使うように変更
    
    * リファクタリング
    
    * 回転行列を使うように
    
    * Update src/ai_server/game/action/receive.cc
    
    Co-authored-by: Kenta Sato <6215720+Tosainu@users.noreply.github.com>
    
    * <Eigen/Geometry>をインクルード
    
    * フィールド内&ペナルティエリア外に目標位置を作るように
    
    * 表記を統一
    
    * ペナルティエリアを避けるかどうかのフラグを用意
    
    * 条件修正
    
    * マージン追加
    
    * ヘッダ整理、コメント追加
    
    * shoot_posはconst参照で受け取るように
    
    * リファクタリング
    
    Co-authored-by: Kenta Sato <6215720+Tosainu@users.noreply.github.com>

--- src/ai_server/game/action/receive.cc
+++ src/ai_server/game/action/receive.cc
@@ -1,5 +1,6 @@
 #include <cmath>
 #include <boost/math/constants/constants.hpp>
+#include <Eigen/Geometry>
 
 #include "ai_server/game/action/receive.h"
 #include "ai_server/util/math/angle.h"
@@ -18,7 +19,8 @@ receive::receive(context& ctx, unsigned int id)
       shoot_flag_(false),
       kick_type_setted_(false),
       kick_type_({model::command::kick_type_t::none, 0}),
-      shoot_pos_(Eigen::Vector2d::Zero()) {}
+      shoot_pos_(Eigen::Vector2d::Zero()),
+      avoid_penalty_(false) {}
 
 void receive::set_dribble(int dribble) {
   dribble_ = dribble;
@@ -32,7 +34,7 @@ void receive::set_passer(unsigned int passer_id) {
 unsigned int receive::passer() {
   return passer_id_;
 }
-void receive::set_shoot(Eigen::Vector2d shoot_pos) {
+void receive::set_shoot(const Eigen::Vector2d& shoot_pos) {
   shoot_pos_  = shoot_pos;
   shoot_flag_ = true;
 }
@@ -41,7 +43,14 @@ void receive::set_kick_type(const model::command::kick_flag_t& kick_type) {
   kick_type_setted_ = true;
 }
 
+void receive::set_avoid_penalty(bool avoid_penalty) {
+  avoid_penalty_ = avoid_penalty;
+}
+
 model::command receive::execute() {
+  const auto wf = world().field();
+  // ロボット半径
+  constexpr double robot_rad = 90.0;
   // 既定のキックパワー
   constexpr int kick_power = 50;
   //それぞれ自機を生成
@@ -49,40 +58,40 @@ model::command receive::execute() {
   const auto our_robots = model::our_robots(world(), team_color());
   if (!our_robots.count(id_)) return command;
   const auto& robot               = our_robots.at(id_);
-  const Eigen::Vector2d robot_vel = util::math::velocity(robot);
   const Eigen::Vector2d robot_pos = util::math::position(robot);
   const Eigen::Vector2d ball_vel  = util::math::velocity(world().ball());
   const Eigen::Vector2d ball_pos  = util::math::position(world().ball());
-  double theta =
+  const double theta =
       shoot_flag_ ? std::atan2(shoot_pos_.y() - robot_pos.y(), shoot_pos_.x() - robot_pos.x())
                   : std::atan2(ball_vel.y(), ball_vel.x()) + pi<double>();
-  const double omega = 4.0 * util::math::wrap_to_pi(theta - robot.theta());
 
   // シュート時にボールの直線から離れる距離
   const double kick_dist = shoot_flag_ ? 100.0 : 0.0;
-  const Eigen::Vector2d kick_pos =
-      robot_pos + kick_dist * Eigen::Vector2d(std::cos(theta), std::sin(theta));
+  const Eigen::Rotation2Dd rot{theta};
+  const Eigen::Vector2d kick_pos = robot_pos + kick_dist * (rot * Eigen::Vector2d::UnitX());
 
   //ボールがめっちゃ近くに来たら受け取ったと判定
   //現状だとボールセンサに反応があるか分からないので
-  if ((robot_pos - ball_pos).norm() < 100) flag_ = true;
+  if ((robot_pos - ball_pos).norm() < 100.0) flag_ = true;
 
-  if (ball_vel.norm() < 500) {
-    command.set_velocity({0.0, 0.0, 0.0});
-  } else {
-    const Eigen::Vector2d normalize = ball_vel.normalized();
-
-    // 基準位置
-    const Eigen::Vector2d base_pos = ball_pos + normalize * (kick_pos - ball_pos).norm();
-    // 目標位置
-    Eigen::Vector2d target_pos =
-        base_pos - kick_dist * Eigen::Vector2d(std::cos(theta), std::sin(theta));
-    // 速度
-    const Eigen::Vector2d vel =
-        4.0 * (target_pos - robot_pos) + ball_vel -
-        (base_pos - ball_pos).normalized() * ball_vel.dot((base_pos - ball_pos).normalized());
-    command.set_velocity({vel.x(), vel.y(), omega});
+  if (ball_vel.norm() >= 500.0) {
+    for (double dist = (kick_pos - ball_pos).norm(); dist > robot_rad; dist -= 50.0) {
+      // 基準位置
+      const Eigen::Vector2d base_pos = ball_pos + dist * ball_vel.normalized();
+      // 目標位置
+      const Eigen::Vector2d pos = base_pos - kick_dist * (rot * Eigen::Vector2d::UnitX());
+      // フィールド内なら
+      if (std::abs(pos.x()) < wf.x_max() + robot_rad &&
+          std::abs(pos.y()) < wf.y_max() + robot_rad &&
+          (!avoid_penalty_ ||
+           std::abs(pos.x()) < wf.x_max() - wf.penalty_length() - robot_rad - 10.0 ||
+           std::abs(pos.y()) > 0.5 * wf.penalty_width() + robot_rad + 10.0)) {
+        command.set_position(pos, theta);
+        break;
+      }
+    }
   }
+
   // kick
   if (kick_type_setted_) {
     command.set_kick_flag(kick_type_);
--- src/ai_server/game/action/receive.h
+++ src/ai_server/game/action/receive.h
@@ -10,15 +10,45 @@ namespace game {
 namespace action {
 class receive : public base {
 public:
+  /// @brief コンストラクタ
+  /// @param ctx コンテキスト
+  /// @param id 使うロボットのid
   receive(context& ctx, unsigned int id);
+
+  /// @brief ドリブルパワーを設定する
+  /// @param dribble ドリブルパワー
   void set_dribble(int dribble);
+
+  /// @brief ドリブルパワーを取得する
+  /// @return ドリブルパワー
   int dribble();
+
+  /// @brief パスしてくる相手を設定する
+  /// @param passer_id パスしてくる相手のID
   void set_passer(unsigned int passer_id);
+
+  /// @brief パスしてくる相手を取得する
+  /// @return パスしてくる相手のID
   unsigned int passer();
+
+  /// @brief 実行
+  /// @return ロボットに送信するコマンド
   model::command execute() override;
-  bool finished() const override;
-  void set_shoot(Eigen::Vector2d shoot_pos);
+
+  /// @brief シュートの目標位置を設定する
+  /// @param shoot_pos シュートの目標位置
+  void set_shoot(const Eigen::Vector2d& shoot_pos);
+
+  /// @brief キックタイプを設定する
+  /// @param kick_type キックタイプ
   void set_kick_type(const model::command::kick_flag_t& kick_type);
+
+  /// @brief ペナルティエリアを避けるかを設定する
+  /// @param avoid_penalty ペナルティエリアを避けるか(デフォルトではfalse)
+  void set_avoid_penalty(bool avoid_penalty);
+
+  /// @return 動作が完了しているか?
+  bool finished() const override;
 
 private:
   int dribble_;
@@ -28,6 +58,7 @@ private:
   bool kick_type_setted_;
   model::command::kick_flag_t kick_type_;
   Eigen::Vector2d shoot_pos_;
+  bool avoid_penalty_;
 };
 } // namespace action
 } // namespace game
