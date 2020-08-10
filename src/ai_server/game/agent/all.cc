0accb053f30c3967eb8360012dda872390833621
YuseiNaito <yusei.vitz.787@gmail.com>
2020-08-11 08:52:45 +0900

    all でキーパーしかいないときに動かない問題を修正 (#606)
    * キーパーしかいないときに動かなかった問題を修正
    
    * 誤字修正
    
    * リファクタリング
    
    * erase していなかったのを修正

--- src/ai_server/game/agent/all.cc
+++ src/ai_server/game/agent/all.cc
@@ -27,7 +27,7 @@ all::all(context& ctx, const std::vector<unsigned int>& ids, unsigned int keeper
           keeper_id, Eigen::Vector2d(world().field().x_max(), 0.0))) {
   const auto now = std::chrono::steady_clock::now();
   const Eigen::Vector2d ene_goal_pos(world().field().x_max(), 0.0);
-  for (const auto id : ids_) {
+  for (auto id : ids_) {
     get_ball_[id]   = make_action<action::get_ball>(id, ene_goal_pos);
     receive_[id]    = make_action<action::receive>(id);
     move_[id]       = make_action<action::move>(id);
@@ -40,7 +40,6 @@ all::all(context& ctx, const std::vector<unsigned int>& ids, unsigned int keeper
 
 std::vector<std::shared_ptr<action::base>> all::execute() {
   std::vector<std::shared_ptr<action::base>> baseaction;
-  if (ids_.empty()) return baseaction;
   const auto wf                  = world().field();
   const Eigen::Vector2d ball_pos = util::math::position(world().ball());
   const Eigen::Vector2d ball_vel = util::math::velocity(world().ball());
@@ -48,6 +47,8 @@ std::vector<std::shared_ptr<action::base>> all::execute() {
   const auto ene_robots          = model::enemy_robots(world(), team_color());
   const Eigen::Vector2d our_goal_pos(wf.x_min(), 0.0);
   const Eigen::Vector2d ene_goal_pos(wf.x_max(), 0.0);
+  if (ids_.empty() && !our_robots.count(keeper_id_)) return baseaction;
+
   // kick power(line)
   constexpr int line_pow = 40;
   // kick power(chip)
@@ -57,9 +58,9 @@ std::vector<std::shared_ptr<action::base>> all::execute() {
   // 障害物としてのロボット半径
   constexpr double obs_robot_rad = 300.0;
   // フィールドから出られる距離
-  constexpr double field_mergin = 200.0;
+  constexpr double field_margin = 200.0;
   // ペナルティエリアから余裕を持たせる距離
-  constexpr double penalty_mergin = 150.0;
+  constexpr double penalty_margin = 150.0;
   // 一般障害物設定
   planner::obstacle_list common_obstacles;
   {
@@ -67,8 +68,8 @@ std::vector<std::shared_ptr<action::base>> all::execute() {
       common_obstacles.add(
           model::obstacle::point{util::math::position(robot.second), obs_robot_rad});
     }
-    common_obstacles.add(model::obstacle::enemy_penalty_area(world().field(), penalty_mergin));
-    common_obstacles.add(model::obstacle::our_penalty_area(world().field(), penalty_mergin));
+    common_obstacles.add(model::obstacle::enemy_penalty_area(world().field(), penalty_margin));
+    common_obstacles.add(model::obstacle::our_penalty_area(world().field(), penalty_margin));
   }
 
   ///////////////////////////////////////////
@@ -88,6 +89,8 @@ std::vector<std::shared_ptr<action::base>> all::execute() {
       robot_pos_.erase(id);
     }
   }
+  auto end = std::remove(visible_ids.begin(), visible_ids.end(), keeper_id_);
+  visible_ids.erase(end, visible_ids.end());
 
   // guardしたいリスト
   std::vector<Eigen::Vector2d> guard_list;
@@ -121,14 +124,14 @@ std::vector<std::shared_ptr<action::base>> all::execute() {
 
   ///////////////////////////////////////////
   // 役割の更新 /////////////////////////////
-  {
+  if (!visible_ids.empty()) {
     auto tmp_ids = visible_ids;
     // chaser /////////////////////////////
     {
       std::vector<unsigned int> candidates;
       if (std::abs(pass_target_.x()) < wf.x_max() - wf.penalty_length() ||
           std::abs(pass_target_.y()) > wf.penalty_width() / 2.0) {
-        for (const auto id : tmp_ids) {
+        for (auto id : tmp_ids) {
           if ((robot_pos_.at(id) - pass_target_).norm() < 500.0 &&
               ball_vel.dot((robot_pos_.at(id) - ball_pos).normalized()) > 1000.0 &&
               std::abs(util::math::wrap_to_pi(
@@ -141,7 +144,7 @@ std::vector<std::shared_ptr<action::base>> all::execute() {
       if (candidates.empty()) {
         std::unordered_map<unsigned int, double> score_list;
         score_list.reserve(tmp_ids.size());
-        for (const auto id : tmp_ids) {
+        for (auto id : tmp_ids) {
           const Eigen::Vector2d br = robot_pos_.at(id) - ball_pos;
           score_list[id] = br.norm() / (std::max(ball_vel.dot(br.normalized()), 0.0) + 2000.0);
         }
@@ -166,7 +169,8 @@ std::vector<std::shared_ptr<action::base>> all::execute() {
   }
 
   // chaser /////////////////////////////
-  if (our_robots.count(chaser_)) {
+  // 無効な chaser が入っている可能性があるため， visible_ids が空でない必要がある
+  if (!visible_ids.empty() && our_robots.count(chaser_)) {
     // MCTSでpass_targetを決定
     const detail::mcts::state state{ball_pos, chaser_};
     detail::mcts::node root_node(state);
@@ -199,7 +203,7 @@ std::vector<std::shared_ptr<action::base>> all::execute() {
     }
     // planner::human_likeを使用
     std::unique_ptr<planner::human_like> hl = std::make_unique<planner::human_like>();
-    hl->set_area(wf, field_mergin);
+    hl->set_area(wf, field_margin);
 
     if (std::abs(ball_pos.x()) > wf.x_max() || std::abs(ball_pos.y()) > wf.y_max()) {
       vec_.at(id)->move_at(0.0, 0.0, 0.0);
@@ -243,7 +247,7 @@ std::vector<std::shared_ptr<action::base>> all::execute() {
   }
 
   // waiter ///////////////////////////
-  {
+  if (!waiters_.empty()) {
     std::unordered_map<unsigned int, Eigen::Vector2d> wait_pos;
     std::vector<Eigen::Vector2d> tmp_poss;
     // defense
@@ -274,7 +278,7 @@ std::vector<std::shared_ptr<action::base>> all::execute() {
                           std::min(2500.0, wf.y_max()));
     tmp_poss.emplace_back(std::min(600.0, wf.x_max() - wf.penalty_length() - 500.0),
                           std::max(-2500.0, wf.y_min()));
-    if (!(tmp_poss.empty()) && (ball_pos - our_goal_pos).norm() > 5000.0) {
+    if (!tmp_poss.empty() && (ball_pos - our_goal_pos).norm() > 5000.0) {
       const auto itr = std::min_element(
           tmp_poss.cbegin(), tmp_poss.cend(),
           [&chaser_pos = robot_pos_.at(chaser_)](const auto& a, const auto& b) {
@@ -297,7 +301,7 @@ std::vector<std::shared_ptr<action::base>> all::execute() {
     }
 
     // action設定
-    for (const auto id : waiters_) {
+    for (auto id : waiters_) {
       if (!our_robots.count(id)) continue;
       const Eigen::Vector2d& robot_pos = robot_pos_.at(id);
       const Eigen::Vector2d& pos =
@@ -311,10 +315,10 @@ std::vector<std::shared_ptr<action::base>> all::execute() {
       }
       // planner::human_likeを使用
       std::unique_ptr<planner::human_like> hl = std::make_unique<planner::human_like>();
-      hl->set_area(wf, field_mergin);
+      hl->set_area(wf, field_margin);
 
-      if (pos.x() < wf.x_min() + wf.penalty_length() + penalty_mergin &&
-          std::abs(pos.y()) < wf.penalty_width() / 2.0 + penalty_mergin) {
+      if (pos.x() < wf.x_min() + wf.penalty_length() + penalty_margin &&
+          std::abs(pos.y()) < wf.penalty_width() / 2.0 + penalty_margin) {
         // 自陣ゴール前で防御
         guard_.at(id)->move_to(pos.x(), pos.y());
         baseaction.push_back(guard_.at(id));
