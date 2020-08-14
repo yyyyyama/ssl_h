69750711e81cd97a3d855c0c22df64eeecc9533c
KoseiNaito <49829516+KoseiNaito@users.noreply.github.com>
2020-08-14 12:23:01 +0900

    alignmentの更新 (#609)
    * alignmentへのplannerの処理の追加とリファクタリング
    
    * includeの順番の修正と障害物半径の変更
    
    * リファクタリング
    
    * リファクタリング
    
    * リファクタリング
    
    * fix

--- src/ai_server/game/agent/alignment.cc
+++ src/ai_server/game/agent/alignment.cc
@@ -1,12 +1,19 @@
 #include <cmath>
+
 #include <boost/math/constants/constants.hpp>
+
+#include "ai_server/game/action/move.h"
+#include "ai_server/game/action/with_planner.h"
+#include "ai_server/model/obstacle/field.h"
+#include "ai_server/planner/human_like.h"
+#include "ai_server/util/math/angle.h"
+#include "ai_server/util/math/to_vector.h"
+
 #include "alignment.h"
 
 using boost::math::constants::pi;
 
-namespace ai_server {
-namespace game {
-namespace agent {
+namespace ai_server::game::agent {
 
 alignment::alignment(context& ctx, const std::vector<unsigned int>& ids)
     : base(ctx), ids_(ids) {}
@@ -14,24 +21,46 @@ alignment::alignment(context& ctx, const std::vector<unsigned int>& ids)
 std::vector<std::shared_ptr<action::base>> alignment::execute() {
   std::vector<std::shared_ptr<action::base>> exe;
   const auto our_robots = model::our_robots(world(), team_color());
+  // 障害物としてのロボット半径
+  constexpr double obs_robot_rad = 200.0;
+  // フィールドから出られる距離
+  constexpr double field_margin = 200.0;
+  // ペナルティエリアから余裕を持たせる距離
+  constexpr double penalty_margin = 150.0;
+  // 一般障害物設定
+  planner::obstacle_list common_obstacles;
+  {
+    for (const auto& robot : model::enemy_robots(world(), team_color())) {
+      common_obstacles.add(
+          model::obstacle::point{util::math::position(robot.second), obs_robot_rad});
+    }
+    common_obstacles.add(model::obstacle::enemy_penalty_area(world().field(), penalty_margin));
+    common_obstacles.add(model::obstacle::our_penalty_area(world().field(), penalty_margin));
+  }
 
-  // move
-  int id_n = 0;
   const double x0 =
       -world().field().x_max() / 2 + 110.0 * (static_cast<double>(ids_.size()) - 1.0);
-  for (auto id : ids_) {
-    if (our_robots.count(id)) {
-      auto move          = make_action<action::move>(id);
-      const double y     = 0;
-      const double x     = x0 - 220.0 * id_n;
-      const double theta = pi<double>() * 1.5;
-      move->move_to(x, y, theta);
-      exe.push_back(move);
+  constexpr double y     = 0;
+  constexpr double theta = pi<double>() * 1.5;
+  for (std::size_t i = 0; i < ids_.size(); ++i) {
+    const auto id = *(ids_.begin() + i);
+    if (!our_robots.count(id)) continue;
+    // 自チームロボットを障害物設定
+    auto obstacles = common_obstacles;
+    for (const auto& robot : our_robots) {
+      if (robot.first == id) continue;
+      obstacles.add(model::obstacle::point{util::math::position(robot.second), obs_robot_rad});
     }
-    id_n++;
+    // planner::human_likeを使用
+    std::unique_ptr<planner::human_like> hl = std::make_unique<planner::human_like>();
+    hl->set_area(world().field(), field_margin);
+
+    const double x = x0 - 220.0 * i;
+    auto move      = make_action<action::move>(id);
+    move->move_to(x, y, theta);
+    exe.push_back(std::make_shared<action::with_planner>(move, std::move(hl), obstacles));
   }
   return exe;
 }
-} // namespace agent
-} // namespace game
-} // namespace ai_server
+
+} // namespace ai_server::game::agent
--- src/ai_server/game/agent/alignment.h
+++ src/ai_server/game/agent/alignment.h
@@ -2,22 +2,24 @@
 #define AI_SERVER_GAME_AGENT_ALIGNMENT_H
 
 #include "base.h"
-#include "ai_server/game/action/move.h"
 
-namespace ai_server {
-namespace game {
-namespace agent {
+namespace ai_server::game::agent {
 
 class alignment : public base {
 public:
+  /// @brief コンストラクタ
+  /// @param ctx ボールやロボットの情報
+  /// @param ids 使うロボットのid
   alignment(context& ctx, const std::vector<unsigned int>& ids);
+
+  /// @brief 実行
+  /// @return ロボットに送信するコマンド
   std::vector<std::shared_ptr<action::base>> execute() override;
 
 private:
   const std::vector<unsigned int> ids_;
 };
-} // namespace agent
-} // namespace game
-} // namespace ai_server
+
+} // namespace ai_server::game::agent
 
 #endif
