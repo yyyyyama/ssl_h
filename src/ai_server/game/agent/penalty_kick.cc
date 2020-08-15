be0e2cfcc589bc246960f22ae7ca7efcd5812c7d
YasutakaTsuruta <36723926+YasutakaTsuruta@users.noreply.github.com>
2020-08-16 01:42:19 +0900

    penalty_kickの更新 (#607)
    * 待機位置を新ルールに対応
    
    * plannerを設定するための準備
    
    * human_likeを実装
    
    * kickerの障害物に自分が入らないように修正
    
    * include順を修正
    
    * 名前空間を１行で記述
    
    * 不要な条件式を削除
    
    * constexprに変更
    
    * ヘッダのコメントを追加
    
    * 使ってなさそうな関数を削除
    
    * 注意書きのコメントを追加
    
    * 不要な条件式を削除
    
    * 空行を追加
    
    * 紛らわしい名前を変更

--- src/ai_server/game/agent/penalty_kick.cc
+++ src/ai_server/game/agent/penalty_kick.cc
@@ -1,10 +1,14 @@
-#include "penalty_kick.h"
-#include "ai_server/util/math/to_vector.h"
 #include <cmath>
 #include <boost/math/constants/constants.hpp>
-namespace ai_server {
-namespace game {
-namespace agent {
+
+#include "ai_server/game/action/with_planner.h"
+#include "ai_server/model/obstacle/field.h"
+#include "ai_server/planner/human_like.h"
+#include "ai_server/util/math/to_vector.h"
+
+#include "penalty_kick.h"
+
+namespace ai_server::game::agent {
 
 penalty_kick::penalty_kick(context& ctx, unsigned int kicker_id,
                            const std::vector<unsigned int>& ids, unsigned int enemy_keeper)
@@ -16,19 +20,11 @@ penalty_kick::penalty_kick(context& ctx, unsigned int kicker_id,
       turn_kick_(make_action<action::turn_kick>(kicker_id)),
       kicker_move_(make_action<action::move>(kicker_id)),
       kicker_id_(kicker_id),
-      keeper_id_(enemy_keeper),
+      enemy_keeper_id_(enemy_keeper),
       change_command_time_(std::chrono::seconds(0)) {}
-
-penalty_kick::penalty_mode penalty_kick::mode() const {
-  return mode_;
-}
 
 void penalty_kick::set_mode(penalty_kick::penalty_mode mode) {
   mode_ = mode;
-}
-
-bool penalty_kick::start_flag() const {
-  return start_flag_;
 }
 
 void penalty_kick::set_start_flag(bool start_flag) {
@@ -40,26 +36,49 @@ bool penalty_kick::finished() {
 }
 
 std::vector<std::shared_ptr<action::base>> penalty_kick::execute() {
-  //////////////////////////////
-  //      キッカーの処理
-  /////////////////////////////
   std::vector<std::shared_ptr<action::base>> exe;
-  const auto our_robots = model::our_robots(world(), team_color());
+  const auto our_robots                  = model::our_robots(world(), team_color());
+  const auto enemy_robots                = model::enemy_robots(world(), team_color());
+  const auto& enemy_keeper               = enemy_robots.at(enemy_keeper_id_);
+  const Eigen::Vector2d enemy_keeper_pos = util::math::position(enemy_keeper);
+  const auto ball                        = util::math::position(world().ball());
+  const auto field                       = world().field();
+  const auto penalty_mark                = field.back_penalty_mark();
+  const auto point                       = std::chrono::steady_clock::now();
+
+  using boost::math::constants::pi;
+
+  // 視認可能なロボットを抽出する
   std::vector<unsigned int> visible_ids;
   std::copy_if(
       ids_.cbegin(), ids_.cend(), std::back_inserter(visible_ids),
       [&our_robots, this](auto id) { return id != kicker_id_ && our_robots.count(id); });
-  const auto enemy_robots          = model::enemy_robots(world(), team_color());
-  const auto& keeper               = enemy_robots.at(keeper_id_);
-  const Eigen::Vector2d keeper_pos = util::math::position(keeper);
-  const auto ball                  = world().ball();
-  const auto field                 = world().field();
-  const auto point                 = std::chrono::steady_clock::now();
 
-  using boost::math::constants::pi;
+  // ロボットの障害物としての半径
+  constexpr double obs_robot_rad = 200.0;
+  // フィールド外に出られる距離
+  constexpr double area_margin = 200.0;
+  // 障害物設定
+  planner::obstacle_list common_obstacles;
+  common_obstacles.add(model::obstacle::our_penalty_area(field, 150.0));
+  for (const auto& robot : enemy_robots) {
+    common_obstacles.add(
+        model::obstacle::point{util::math::position(robot.second), obs_robot_rad});
+  }
 
+  //////////////////////////////
+  //      キッカーの処理
+  /////////////////////////////
   //攻撃側が指定されているとき
   if (mode_ == penalty_kick::penalty_mode::attack && our_robots.count(kicker_id_)) {
+    auto hl = std::make_unique<planner::human_like>();
+    hl->set_area(field, area_margin);
+    auto obstacles = common_obstacles;
+    for (const auto& robot : our_robots) {
+      if (robot.first != kicker_id_)
+        obstacles.add(
+            model::obstacle::point{util::math::position(robot.second), obs_robot_rad});
+    }
     switch (state_) {
       // wait - 待機位置に移動して待機
       case attack_state::wait:
@@ -67,8 +86,11 @@ std::vector<std::shared_ptr<action::base>> penalty_kick::execute() {
           state_               = attack_state::check;
           change_command_time_ = point;
         }
-        kicker_move_->move_to(field.x_max() - field.penalty_length() - 600.0, 0.0, 0.0);
-        exe.push_back(kicker_move_);
+        kicker_move_->move_to(penalty_mark.x - 600.0, penalty_mark.y, 0.0);
+        obstacles.add(model::obstacle::enemy_penalty_area(field, 150.0));
+        obstacles.add(model::obstacle::point{ball, 500.0});
+        exe.push_back(
+            std::make_shared<action::with_planner>(kicker_move_, std::move(hl), obstacles));
         break;
 
       // check - 状況に合わせて指定位置を変更
@@ -82,7 +104,7 @@ std::vector<std::shared_ptr<action::base>> penalty_kick::execute() {
         turn_kick_->set_start_pos({ball.x() - 80.0, ball.y()});
 
         // キーパーに合わせて位置を指定
-        if (keeper_pos.y() < 0) {
+        if (enemy_keeper_pos.y() < 0) {
           turn_kick_->set_angle(pi<double>() / 7);
           turn_kick_->set_radius(90.0);
         } else {
@@ -94,13 +116,16 @@ std::vector<std::shared_ptr<action::base>> penalty_kick::execute() {
         if (turn_kick_->wait()) {
           state_ = attack_state::kick;
         }
-        exe.push_back(turn_kick_);
+
+        exe.push_back(
+            std::make_shared<action::with_planner>(turn_kick_, std::move(hl), obstacles));
         break;
 
       // kick - キック
       case attack_state::kick:
         turn_kick_->set_kick(true);
-        exe.push_back(turn_kick_);
+        exe.push_back(
+            std::make_shared<action::with_planner>(turn_kick_, std::move(hl), obstacles));
         break;
     }
   } else if (mode_ == penalty_kick::penalty_mode::defense && our_robots.count(kicker_id_)) {
@@ -110,6 +135,7 @@ std::vector<std::shared_ptr<action::base>> penalty_kick::execute() {
   //////////////////////////////
   //      キッカー以外の処理
   //////////////////////////////
+  if (visible_ids.empty()) return exe;
   //パラメータ設定
   int count             = 2;                               //何番目のロボットか判別
   const double interval = ball.x() < 0 ? 500.0 : -500.0;   //ロボットの間隔
@@ -117,18 +143,26 @@ std::vector<std::shared_ptr<action::base>> penalty_kick::execute() {
 
   //ちょうどいい位置に並べる
   for (const auto id : visible_ids) {
+    auto hl = std::make_unique<planner::human_like>();
+    hl->set_area(field, area_margin);
+    auto obstacles = common_obstacles;
+    for (const auto& robot : our_robots) {
+      if (robot.first != id)
+        obstacles.add(
+            model::obstacle::point{util::math::position(robot.second), obs_robot_rad});
+    }
+    obstacles.add(model::obstacle::enemy_penalty_area(field, 150.0));
+    obstacles.add(model::obstacle::point{ball, 600.0});
     const double x = line + interval * (count / 2);
     const double y =
         (count % 2 == 1) ? field.y_min() + 500.0 : field.y_max() - 500.0; //順番に左右に分ける
-    const auto move = make_action<action::move>(id);
+    auto move = make_action<action::move>(id);
     move->move_to(x, y, 0);
-    exe.push_back(move);
+    exe.push_back(std::make_shared<action::with_planner>(move, std::move(hl), obstacles));
     count++;
   }
 
   return exe;
 }
 
-} // namespace agent
-} // namespace game
-} // namespace ai_server
+} // namespace ai_server::game::agent
--- src/ai_server/game/agent/penalty_kick.h
+++ src/ai_server/game/agent/penalty_kick.h
@@ -8,27 +8,48 @@
 #include <memory>
 #include <vector>
 
-namespace ai_server {
-namespace game {
-namespace agent {
+///
+/// このagentはpenalty時にペナルティライン上からキックするルールで使用されていたものです。
+/// ルール変更後は適応した別のagentを使うことを推奨します。
+///
+
+namespace ai_server::game::agent {
 
 class penalty_kick : public base {
 public:
+  /// @brief コンストラクタ
+  /// @param ctx ボールやロボットの情報
+  /// @param kicker_id キッカーにするロボットのid
+  /// @param ids キッカー以外のロボットのid
+  /// @param enemy_keeper 敵キーパーのid
   penalty_kick(context& ctx, unsigned int kicker_id, const std::vector<unsigned int>& ids,
                unsigned int enemy_keeper);
-  enum class penalty_mode { attack, defense }; //攻撃側と守備側で変える
 
-  penalty_kick::penalty_mode mode() const;
-  void set_mode(penalty_kick::penalty_mode);
-  bool start_flag() const;
-  void set_start_flag(bool start_flag);
+  /// @brief 実行
+  /// @return ロボットに送信するコマンド
   std::vector<std::shared_ptr<action::base>> execute() override;
+
+  enum class penalty_mode {
+    attack, ///< 攻撃側
+    defense ///< 守備側
+  };
+
+  /// @brief 攻撃側か守備側かを設定する
+  /// @param mode 設定するpenalty_mode
+  void set_mode(penalty_kick::penalty_mode mode);
+
+  /// @brief スタートしているかを設定する
+  /// @param start_flag スタートしているか
+  void set_start_flag(bool start_flag);
+
+  /// @brief penalty_kickの動作が終了したか取得する
+  /// @return 終了していたらtrue
   bool finished();
 
 private:
-  enum class attack_state { wait, check, kick }; //攻撃の状態
-  attack_state state_;
+  enum class attack_state { wait, check, kick };
 
+  attack_state state_;
   penalty_mode mode_;
 
   bool start_flag_;
@@ -38,13 +59,12 @@ private:
 
   unsigned int kicker_id_;
 
-  //敵キーパーの動きを監視して蹴るタイミングを変える
-  unsigned int keeper_id_;
+  // 敵キーパーの動きを監視して蹴るタイミングを変える
+  unsigned int enemy_keeper_id_;
 
   std::chrono::steady_clock::time_point change_command_time_; // PKの経過時間
 };
-} // namespace agent
-} // namespace game
-} // namespace ai_server
+
+} // namespace ai_server::game::agent
 
 #endif
